397259d367137913cb7f13f8a5523155d80f4eff
Vlad Spuskaniuk <beard13243598@gmail.com>
2019-08-05 19:10:24 +0300

    Fixed_Show

--- resources/views/admin/news/edit.blade.php
+++ resources/views/admin/news/edit.blade.php
@@ -60,5 +60,5 @@
                     });
                 });
             </script>
-    @endpush
-</div>
+        @endpush
+    </div>
--- resources/views/admin/news/show.blade.php
+++ resources/views/admin/news/show.blade.php
@@ -1,4 +1,3 @@
-
 @extends('layouts.app')
 
 @section('content')
@@ -11,13 +10,26 @@
                     <div class="card-header">News {{ $news->id }}</div>
                     <div class="card-body">
 
-                        <a href="{{ url('/admin/news') }}" title="Back"><button class="btn btn-warning btn-sm"><i class="fa fa-arrow-left" aria-hidden="true"></i> Back</button></a>
-                        <a href="{{ url('/admin/news/' . $news->id . '/edit') }}" title="Edit News"><button class="btn btn-primary btn-sm"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</button></a>
+                        <a href="{{ url('/admin/news') }}" title="Back">
+                            <button class="btn btn-warning btn-sm"><i class="fa fa-arrow-left" aria-hidden="true"></i>
+                                Back
+                            </button>
+                        </a>
+                        <a href="{{ url('/admin/news/' . $news->id . '/edit') }}" title="Edit News">
+                            <button class="btn btn-primary btn-sm"><i class="fa fa-pencil-square-o"
+                                                                      aria-hidden="true"></i> Edit
+                            </button>
+                        </a>
 
-                        <form method="POST" action="{{ url('admin/news' . '/' . $news->id) }}" accept-charset="UTF-8" style="display:inline">
+                        <form method="POST" action="{{ url('admin/news' . '/' . $news->id) }}" accept-charset="UTF-8"
+                              style="display:inline">
                             {{ method_field('DELETE') }}
                             {{ csrf_field() }}
-                            <button type="submit" class="btn btn-danger btn-sm" title="Delete News" onclick="return confirm(&quot;Confirm delete?&quot;)"><i class="fa fa-trash-o" aria-hidden="true"></i> Delete</button>
+                            <button type="submit" class="btn btn-danger btn-sm" title="Delete News"
+                                    onclick="return confirm(&quot;Confirm delete?&quot;)"><i class="fa fa-trash-o"
+                                                                                             aria-hidden="true"></i>
+                                Delete
+                            </button>
                         </form>
 
                         <br/>
@@ -33,61 +45,79 @@
                                     <table class="table">
                                         <tbody>
                                         <tr>
-                                            <th>ID</th><td>{{ $news->id }}</td>
+                                            <th>ID</th>
+                                            <td>{{ $news->id }}</td>
                                         </tr>
-                                        <tr><th> Name </th><td> {{ $news->name }} </td></tr>
-                                        <tr><th> Active </th><td> {{ $news->active }} </td></tr>
-                                        <tr><th> Publish Date </th><td> {{ $news->publish_date }} </td></tr>
-                                        <tr><th>  </th><td> </td></tr>
+                                        <tr>
+                                            <th> Name</th>
+                                            <td> {{ $news->name }} </td>
+                                        </tr>
+                                        <tr>
+                                            <th> Active</th>
+                                            <td> {{ $news->active }} </td>
+                                        </tr>
+                                        <tr>
+                                            <th> Publish Date</th>
+                                            <td> {{ $news->publish_date }} </td>
+                                        </tr>
+                                        <tr>
+                                            <th></th>
+                                            <td></td>
+                                        </tr>
                                         </tbody>
                                     </table>
                                 </div>
                             </div>
 
-                        @foreach(config('translatable.locales') as $locale)
+                            @foreach(config('translatable.locales') as $locale)
 
-                            <div class="tab-pane fade" id={{$locale}} role="tabpanel"
-                                 aria-labelledby={{$locale}}>
+                                <div class="tab-pane fade" id={{$locale}} role="tabpanel"
+                                     aria-labelledby={{$locale}}>
 
 
-                                     <tr><th>
-                                        @isset($news->translate($locale)->title)
+                                    <tr>
+                                        <th>
+                                            @isset($news->translate($locale)->title)
 
-                                            <h3><b>Title</b>  ({{$locale}}) </h3>
-                                            <h4 class="bg-light" @if($locale == 'ar') dir="rtl" class="text-right" @endif>
-                                                {{ $news->translate($locale)->title }}
-                                            </h4>
-                                        @endisset
-                                     </th><td>
-
-                                     <tr><th>
-
-                                        @isset($news->translate($locale)->text)
-
-                                            <h4> <b>Text ({{$locale}})</b>  </h4>
-
-                                        <div class="container-fluid bg-light">
-
-                                                <p @if($locale == 'ar') dir="rtl" class="text-right" @endif>
-                                                    {!! $news->translate($locale)->text !!}
-                                                </p>
-
+                                                <h3><b>Title</b> ({{$locale}}) </h3>
+                                                <h4 class="bg-light" @if($locale == 'ar') dir="rtl"
+                                                    class="text-right" @endif>
+                                                    {{ $news->translate($locale)->title }}
+                                                </h4>
                                             @endisset
+                                        </th>
+                                        <td>
 
-                                        </div>
+                                    <tr>
+                                        <th>
+
+                                            @isset($news->translate($locale)->text)
+
+                                                <h4><b>Text ({{$locale}})</b></h4>
+
+                                                <div class="container-fluid bg-light">
+
+                                                    <p @if($locale == 'ar') dir="rtl" class="text-right" @endif>
+                                                        {!! $news->translate($locale)->text !!}
+                                                    </p>
+
+                                                    @endisset
+
+                                                </div>
 
 
-                                    </th><td>
+                                        </th>
+                                        <td>
 
-                            </div>
+                                </div>
 
-                        @endforeach
+                            @endforeach
 
                         </div>
                     </div>
 
-                    </div>
                 </div>
+            </div>
             @include('admin.langPanel')
         </div>
     </div>

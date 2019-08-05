a60550af2e7ba11099f5aa492c849231286e480d
Vlad Spuskaniuk <beard13243598@gmail.com>
2019-08-05 19:55:31 +0300

    Fixed_Show

--- resources/views/admin/news/show.blade.php
+++ resources/views/admin/news/show.blade.php
@@ -75,44 +75,35 @@
                                      aria-labelledby={{$locale}}>
 
 
-                                    <tr>
-                                        <th>
-                                            @isset($news->translate($locale)->title)
+                                            <div class="container">
 
-                                                <h3><b>Title</b> ({{$locale}}) </h3>
-                                                <h4 class="bg-light" @if($locale == 'ar') dir="rtl"
-                                                    class="text-right" @endif>
-                                                    {{ $news->translate($locale)->title }}
-                                                </h4>
-                                            @endisset
-                                        </th>
-                                        <td>
-
-                                    <tr>
-                                        <th>
-
-                                            @isset($news->translate($locale)->text)
-
-                                                <h4><b>Text ({{$locale}})</b></h4>
-
-                                                <div class="container-fluid bg-light">
-
-                                                    <p @if($locale == 'ar') dir="rtl" class="text-right" @endif>
-                                                        {!! $news->translate($locale)->text !!}
-                                                    </p>
-
+                                                <div class="row">
+                                                    <div class="mt-3 bg-light rounded pt-3">
+                                                    @isset($news->translate($locale)->title)
+                                                    <div class="col-md-3"><h4><b>Title({{$locale}})</b></h4> </div>
+                                                    <div class="col">
+                                                        <h4 @if($locale == 'ar') dir="rtl"
+                                                                         class="text-right" @endif>
+                                                            {{ $news->translate($locale)->title }}
+                                                        </h4>
+                                                    </div>
                                                     @endisset
-
+                                                        <hr>
+                                                    <div class="w-100"></div>
+                                                        @isset($news->translate($locale)->text)
+                                                    <div class="col-md-3"><h4><b>Text ({{$locale}})</b></h4></div>
+                                                    <div class="col">
+                                                        <p @if($locale == 'ar') dir="rtl" class="text-right" @endif>
+                                                            {!! $news->translate($locale)->text !!}
+                                                        </p>
+                                                    </div>
+                                                        @endisset
                                                 </div>
-
-
-                                        </th>
-                                        <td>
+                                                </div>
+                                            </div>
 
                                 </div>
-
                             @endforeach
-
                         </div>
                     </div>
 

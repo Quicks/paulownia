5598c60f9494788fcad58b86358e595fb920a9ab
Smirnova <smirnova270694@gmail.com>
2019-08-08 22:34:09 +0300

    changed admin form validation

--- resources/views/layouts/admin_form_validator.blade.php
+++ resources/views/layouts/admin_form_validator.blade.php
@@ -8,21 +8,18 @@
             var isOneLanguagefilled = false;
             var allLangArr = {!!json_encode(config('translatable.locales'))!!};
             $.each(allLangArr, function (index, value) {
-                var currentLang = value;
-                $("div#" + currentLang).each(function () {
-                    var empty = $(this).find('@foreach(config('translatable.locales') as $locale)[name^={{$locale}}]@if(!$loop->last),@endif @endforeach').filter(function () {
-                        return $(this).val().trim() === "";
-                    });
-                    if (!empty.length) {
-                        isOneLanguagefilled = true;
-                        return false;
-                    }
+                var empty = $('[name^='+ value +']').filter(function () {
+                    return $(this).val().trim() === "";
                 });
+                if (!empty.length) {
+                    isOneLanguagefilled = true;
+                    return false;
+                }
             });
             if (!isOneLanguagefilled) {
                 event.preventDefault();
                 alert('All fields must be filled in at least one language!');
             }
-        }
+        };
     });
 </script>

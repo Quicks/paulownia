f442c24f9984e8b49b9725a9b62915c8009a0574
Smirnova <smirnova270694@gmail.com>
2019-08-22 22:44:20 +0300

    Added billing template export to doc

--- routes/web.php
+++ routes/web.php
@@ -44,6 +44,7 @@ Route::group(['middleware' => ['web']], function () {
 
 
         Route::group(['middleware' => ['admin']], function () {
+            Route::view('/factura', "admin.factura");
             Route::view('/welcome', 'admin.dashboard')->name('admin.welcome');
             Route::resource('news', 'App\Http\Controllers\Admin\\NewsController');
             Route::resource('galleries', 'App\Http\Controllers\Admin\\GalleriesController');

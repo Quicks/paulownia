0e42e634aa5cc1ff94a46b66afa828cd0b4e6c5f
Vlad Spuskaniuk <beard13243598@gmail.com>
2019-08-30 22:01:44 +0300

    Fixed LangPanel

--- resources/views/admin/langPanel.blade.php
+++ resources/views/admin/langPanel.blade.php
@@ -10,7 +10,7 @@
             <div class="nav flex-column nav-pills " id="v-pills-tab" role="tablist"
                  aria-orientation="vertical">
 
-                <a class="nav-link btn btn-outline-primary btn-sm active"
+                <a class="nav-link btn btn-outline-primary btn-sm active mb-2"
                    id="main-form-btn" data-toggle="pill"
                    href="#main-form" role="tab"
                    aria-controls="main-form" aria-selected="true"
@@ -23,7 +23,7 @@
 
 
                     <br>
-                    <a class="nav-link btn btn-outline-primary btn-sm"
+                    <a class="nav-link btn btn-outline-primary btn-sm mb-2 mt-2"
                        id={{$locale}} data-toggle="pill"
                        href="#{{$locale}}" role="tab"
                        aria-controls={{$locale}} aria-selected=@if ($loop->first)"true" @else "false" @endif

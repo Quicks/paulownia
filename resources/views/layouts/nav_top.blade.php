8c4f420f9fb2236d7e80c4a284cbebaa6314cbb4
Smirnova <smirnova270694@gmail.com>
2019-09-07 15:44:37 +0300

    added nav-top for stats

--- resources/views/layouts/nav_top.blade.php
+++ resources/views/layouts/nav_top.blade.php
@@ -1,59 +1,52 @@
-<nav class="navbar navbar-expand-md navbar-light navbar-laravel">
-    <div class="container-fluid">
-        <a class="navbar-brand" href="{{ url('/admin/welcome') }}">
-            <img src="{{ asset('images/logo.png') }}" width="112px" height="41px" alt="Paulownia"/>
-        </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
+<nav class="navbar navbar-expand-md navbar-light navbar-laravel custom-nav-bs4">
+    <a class="navbar-brand custom-nav-link-bs4" href="{{ url('/admin/welcome') }}">
+        <img src="{{ asset('images/logo.png') }}" width="112px" height="41px" alt="Paulownia"/>
+    </a>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <!-- Left Side Of Navbar -->
+        <ul class="navbar-nav mr-auto">
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <!-- Left Side Of Navbar -->
-            <ul class="navbar-nav mr-auto">
+        </ul>
 
-            </ul>
+        <!-- Right Side Of Navbar -->
+        <ul class="navbar-nav ml-auto">
+            <!-- Authentication Links -->
+            @if(!auth()->guard('admin')->check())
+                <li><a class="nav-link" href="{{ url('/admin/login') }}">Login</a></li>
+                <li><a class="nav-link" href="{{ url('/customer/register') }}">Register</a></li>
+            @else
+                <li class="nav-item dropdown">
+                    <a id="navbarDropdown" class="nav-link" href="#" role="button"
+                       data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        <div class="custom-profile">
+                            <span class="user-name">
+                                {{ auth()->guard('admin')->user()->name }}
+                            </span>
 
-            <!-- Right Side Of Navbar -->
-            <ul class="navbar-nav ml-auto">
-                <!-- Authentication Links -->
-                @if(!auth()->guard('admin')->check())
-                    <li><a class="nav-link" href="{{ url('/admin/login') }}">Login</a></li>
-                    <li><a class="nav-link" href="{{ url('/customer/register') }}">Register</a></li>
-                @else
-                    <li class="nav-item dropdown">
-                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                           data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                            <div class="custom-profile">
-                                        <span class="user-name">
-                                            {{ auth()->guard('admin')->user()->name }}
-                                        </span>
-
-                                <span class="user-role">
-                                            {{ auth()->guard('admin')->user()->role['name'] }}
-                                        </span>
-                            </div>
-                        </a>
-                        <div class="dropdown-menu custom-drop-menu" aria-labelledby="navbarDropdown">
-                            <label class="drop-list-label">
-                                Account
-                            </label>
-                            <ul>
-                                <li>
-                                    <a href="{{ route('shop.home.index') }}"
-                                       target="_blank">{{ trans('admin::app.layouts.visit-shop') }}</a>
-                                </li>
-                                <li>
-                                    <a href="{{ route('admin.account.edit') }}">{{ trans('admin::app.layouts.my-account') }}</a>
-                                </li>
-                                <li>
-                                    <a href="{{ route('admin.session.destroy') }}">{{ trans('admin::app.layouts.logout') }}</a>
-                                </li>
-                            </ul>
+                            <span class="user-role">
+                                {{ auth()->guard('admin')->user()->role['name'] }}
+                            </span>
                         </div>
-                    </li>
-                @endif
-            </ul>
-        </div>
+                            <i class="fa fa-caret-down fa-lg custom-drop-down-icon" aria-hidden="true"></i>
+                    </a>
+                    <div class="dropdown-menu custom-drop-menu" aria-labelledby="navbarDropdown">
+                        <label class="drop-list-label">
+                            Account
+                        </label>
+                        <ul>
+                            <li>
+                                <a href="{{ route('shop.home.index') }}" target="_blank">{{ trans('admin::app.layouts.visit-shop') }}</a>
+                            </li>
+                            <li>
+                                <a href="{{ route('admin.account.edit') }}">{{ trans('admin::app.layouts.my-account') }}</a>
+                            </li>
+                            <li>
+                                <a href="{{ route('admin.session.destroy') }}">{{ trans('admin::app.layouts.logout') }}</a>
+                            </li>
+                        </ul>
+                    </div>
+                </li>
+            @endif
+        </ul>
     </div>
 </nav>
---
+++ resources/views/layouts/nav_top_bootstrap_3.blade.php
@@ -0,0 +1,50 @@
+<nav class="navbar custom-nav-bs3" role="navigation">
+    <div class="container-fluid">
+
+        <a class="navbar-brand custom-nav-link-bs3" href="{{ url('/admin/welcome') }}">
+            <img src="{{ asset('images/logo.png') }}" width="112px" height="41px" alt="Paulownia"/>
+        </a>
+        <div>
+            <ul class="navbar-nav navbar-right custom-list-ul">
+                <!-- Authentication Links -->
+                @if(!auth()->guard('admin')->check())
+                    <li><a class="nav-link" href="{{ url('/admin/login') }}">Login</a></li>
+                    <li><a class="nav-link" href="{{ url('/customer/register') }}">Register</a></li>
+                @else
+                    <li class="nav-item dropdown">
+                        <a id="navbarDropdown" class="dropdown-toggle custom-link" href="#" role="button"
+                           data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                            <div class="custom-profile">
+                                <span class="user-name">
+                                    {{ auth()->guard('admin')->user()->name }}
+                                </span>
+
+                                <span class="user-role">
+                                    {{ auth()->guard('admin')->user()->role['name'] }}
+                                </span>
+                            </div>
+                            <i class="fa fa-caret-down fa-lg custom-drop-down-icon" aria-hidden="true"></i>
+                        </a>
+                        <div class="dropdown-menu custom-drop-menu custom-drop-bs3" aria-labelledby="navbarDropdown">
+                            <label class="drop-list-label">
+                                Account
+                            </label>
+                            <ul>
+                                <li>
+                                    <a href="{{ route('shop.home.index') }}"
+                                       target="_blank">{{ trans('admin::app.layouts.visit-shop') }}</a>
+                                </li>
+                                <li>
+                                    <a href="{{ route('admin.account.edit') }}">{{ trans('admin::app.layouts.my-account') }}</a>
+                                </li>
+                                <li>
+                                    <a href="{{ route('admin.session.destroy') }}">{{ trans('admin::app.layouts.logout') }}</a>
+                                </li>
+                            </ul>
+                        </div>
+                    </li>
+                @endif
+            </ul>
+        </div>
+    </div>
+</nav>

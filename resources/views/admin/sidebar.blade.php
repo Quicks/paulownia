6683644f210a81fa10db094e84f2c444ec6e75a8
Smirnova <smirnova270694@gmail.com>
2019-09-10 23:39:11 +0300

    changed nav left

--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -1,39 +1,39 @@
-<div class="col-md-2">
-    <div class="card">
-        <div class="card-header">
-            Sidebar
-        </div>
-
-        <div class="card-body">
-            <ul class="nav flex-column nav-pills">
-                <li class="nav-item">
-                    <a class="nav-link {{Request::is('admin/welcome*') ? "active" : ""}}" href="{{ url('/admin') }}">
-                        Welcome
-                    </a>
-                </li>
-                <li class="nav-item"><a class="nav-link" href="{{route('admin.dashboard.index')}}">Shop</a></li>
-                <li class="nav-item"><a class="nav-link {{Request::is('admin/news*') ? "active" : ""}}" href="{{route('news.index')}}">News</a></li>
-                <li class="nav-item">
-                    <a class="nav-link {{Request::is('admin/galleries*') ? "active" : ""}}" href="{{route('galleries.index')}}">
-                        Galleries
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link {{Request::is('admin/treatises*') ? "active" : ""}}" href="{{route('treatises.index')}}">
-                        Treatises
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link {{Request::is('admin/offices*') ? "active" : ""}}" href="{{route('offices.index')}}">
-                        Official Offices
-                    </a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link {{Request::is('stats*') ? "active" : ""}}" href="{{route('tracker.stats.index')}}">
-                        Tracking statistics
-                    </a>
-                </li>
-            </ul>
-        </div>
+<div class="col-md-1">
+    <div class="custom-admin-sidebar">
+        <ul>
+            <li>
+                <a href="{{ url('/admin/welcome') }}"><i class="fa fa-home fa-3x"></i><span>Welcome</span></a>
+            </li>
+            <li>
+                <a href="{{route('admin.dashboard.index')}}"><i class="fa fa-shopping-cart fa-3x"></i><span>Shop</span></a>
+            </li>
+            <li>
+                <a href="{{route('news.index')}}"><i class="fa fa-newspaper-o fa-3x"></i><span>News</span></a>
+            </li>
+            <li>
+                <a href="{{route('galleries.index')}}"><i class="fa fa-picture-o fa-3x"></i><span>Galleries</span></a>
+            </li>
+            <li>
+                <a href="{{route('treatises.index')}}"><i class="fa fa-book fa-3x"></i><span>Treatises</span></a>
+            </li>
+            <li>
+                <a href="{{route('offices.index')}}"><i class="fa fa-university fa-3x"></i><span>Official Offices</span></a>
+            </li>
+            <li>
+                <a href="{{route('tracker.stats.index')}}"><i class="fa fa-line-chart fa-3x"></i><span>Tracking statistics</span></a>
+            </li>
+        </ul>
     </div>
 </div>
+
+@push('scripts')
+    <script>
+        $(document).ready(function(){
+            $('.custom-admin-sidebar ul li a').each(function () {
+                if ($(this).attr("href")==window.location.href){
+                    $(this).toggleClass('custom-admin-sidebar-a-active');
+                }
+            });
+        });
+    </script>
+@endpush

660fbb72742c7df005159b28f8e73114d39256cf
s.stepaiko <stepaykosj@gmail.com>
2019-09-12 13:00:50 +0300

    Bug#72,#37 - commented lines deleted

--- resources/views/admin/add-images/add_image_form.blade.php
+++ resources/views/admin/add-images/add_image_form.blade.php
@@ -120,20 +120,6 @@
             $('#image-input-div').remove();
         });
 
-        // function validForm() {
-        //     var isOneLanguagefilled = false;
-        //     $.each(allLangArr, function (index, value) {
-        //         var empty = $('[name^='+ value +']').filter(function () {
-        //             return $(this).val().trim() === "";
-        //         });
-        //         if (!empty.length) {
-        //             isOneLanguagefilled = true;
-        //             return false;
-        //         }
-        //     });
-        //     return isOneLanguagefilled;
-        // };
-
     });
 </script>
 @endpush

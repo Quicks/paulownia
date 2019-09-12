a7c27646393b887046ede0bfb29183c1af994218
s.stepaiko <stepaykosj@gmail.com>
2019-09-12 14:26:04 +0300

    Task#68 add admin menu icons in shop left menu - done

--- packages/Webkul/Admin/src/Resources/views/layouts/master.blade.php
+++ packages/Webkul/Admin/src/Resources/views/layouts/master.blade.php
@@ -11,6 +11,7 @@
         <link rel="stylesheet" href="{{ asset('vendor/webkul/admin/assets/css/admin.css') }}">
         <link rel="stylesheet" href="{{ asset('vendor/webkul/ui/assets/css/ui.css') }}">
         <link href="{{ asset('css/admin_custom.css') }}" rel="stylesheet">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
         @yield('head')
 

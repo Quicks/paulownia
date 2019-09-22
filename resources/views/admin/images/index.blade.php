5dd9c370765b2fa0fcd816e385d6e94c4d0dd6c9
Smirnova <smirnova270694@gmail.com>
2019-09-22 12:46:13 +0300

    changed images blade

--- resources/views/admin/images/index.blade.php
+++ resources/views/admin/images/index.blade.php
@@ -9,10 +9,10 @@
                 <div class="card">
                     <div class="card-header">Images</div>
                     <div class="card-body">
-                        <button class="btn btn-info btn-sm dropdown dropdown-toggle" type="button"
+                        <button class="btn btn-info btn-sm dropdown dropdown-toggle float-right" type="button"
                                 id="dropdownMenuButton" data-toggle="dropdown"
                                 aria-haspopup="true" aria-expanded="false">
-                            Type
+                            Filter
                         </button>
                         <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
                             <a class="dropdown-item"
@@ -26,68 +26,107 @@
                                 </a>
                             @endforeach
                         </div>
-                        <hr>
-                        <div class="col text-center">
-                            @foreach($images as $img)
-                                <button type="button"
-                                        data-toggle="modal"
-                                        data-target="#exampleModal{{$loop->iteration}}"
-                                        class="img-thumbnail"
-                                        style="margin: 2px">
-                                    <img width="150px" height="150px"
-                                        @if($img->imageable_type == 'App\Models\Product')
-                                        src="{{asset('cache/medium/'.$img->path)}}"
-                                        @endif
-                                        src="{{asset('storage/'.$img->image)}}">
-                                </button>
-                                <div class="modal fade" id="exampleModal{{$loop->iteration}}" tabindex="-1"
-                                     role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                                    <div class="modal-dialog modal-xl" role="document">
-                                        <div class="modal-content">
-                                            <div class="modal-header">
-                                                <h5 class="modal-title" id="exampleModalLabel">Image {{$img->id}}</h5>
-                                            </div>
-                                            <div class="modal-body">
-                                                <div class="table-responsive">
-                                                    <table class="table">
-                                                        <tbody>
-                                                        <tr>
-                                                            <th>Type</th>
-                                                            <td>{{substr($img->imageable_type, 11)}}</td>
-                                                        </tr>
-                                                        <tr>
-                                                            <th>{{substr($img->imageable_type, 11) . ' Id'}}</th>
-                                                            @if($img->imageable_type == 'App\Models\Product')
-                                                                <td>{{$img->product_id}}</td>
-                                                            @endif
-                                                                <td>{{$img->imageable_id}}</td>
-                                                        </tr>
-                                                        <tr>
-                                                            <th> URL</th>
-                                                            @if($img->imageable_type == 'App\Models\Product')
-                                                                <td>{{asset('cache/medium/'.$img->path)}}</td>
-                                                            @else
-                                                                <td> {{asset('/storage/' . $img->image)}}</td>
-                                                            @endif
-                                                        </tr>
-                                                        @if($img->imageable_type != 'App\Models\Product')
-                                                        <tr>
-                                                            <th> Created</th>
-                                                            <td> {{$img->created_at}}</td>
-                                                        </tr>
-                                                        @endif
-                                                        </tbody>
-                                                    </table>
-                                                </div>
-                                            </div>
-                                            <div class="modal-footer">
-                                                <button type="button" class="btn btn-info" data-dismiss="modal">Close</button>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            @endforeach
+                        <br/>
+                        <br/>
+
+                        <div class="table-responsive">
+                            <table class="table">
+                                <thead>
+                                <tr>
+                                    <th>#</th><th>Images</th><th>URL</th><th>Type</th>
+                                </tr>
+                                </thead>
+                                <tbody>
+                                    @foreach($images as $img)
+                                        <tr>
+                                            <td>{{ $loop->iteration }}</td>
+                                            <td>
+                                                <img width="100px" height="100px"
+                                                     @if($img->imageable_type == 'App\Models\Product')
+                                                     src="{{asset('cache/medium/'.$img->path)}}"
+                                                     @endif src="{{asset('storage/'.$img->image)}}">
+                                            </td>
+                                            @if($img->imageable_type == 'App\Models\Product')
+                                            <td>{{asset('cache/medium/'.$img->path)}}</td>
+                                            @else
+                                            <td> {{asset('/storage/' . $img->image)}}</td>
+                                            @endif
+                                            <td>{{substr($img->imageable_type, 11)}}</td>
+
+                                        </tr>
+
+                                    @endforeach
+                                 </tbody>
+                            </table>
+
                         </div>
+
+
+
+
+
+
+
+
+                {{--@foreach($images as $img)--}}
+                                {{--<button type="button"--}}
+                                        {{--data-toggle="modal"--}}
+                                        {{--data-target="#exampleModal{{$loop->iteration}}"--}}
+                                        {{--class="img-thumbnail"--}}
+                                        {{--style="margin: 2px">--}}
+                                    {{--<img width="150px" height="150px"--}}
+                                        {{--@if($img->imageable_type == 'App\Models\Product')--}}
+                                        {{--src="{{asset('cache/medium/'.$img->path)}}"--}}
+                                        {{--@endif--}}
+                                        {{--src="{{asset('storage/'.$img->image)}}">--}}
+                                {{--</button>--}}
+                                {{--<div class="modal fade" id="exampleModal{{$loop->iteration}}" tabindex="-1"--}}
+                                     {{--role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">--}}
+                                    {{--<div class="modal-dialog modal-xl" role="document">--}}
+                                        {{--<div class="modal-content">--}}
+                                            {{--<div class="modal-header">--}}
+                                                {{--<h5 class="modal-title" id="exampleModalLabel">Image {{$img->id}}</h5>--}}
+                                            {{--</div>--}}
+                                            {{--<div class="modal-body">--}}
+                                                {{--<div class="table-responsive">--}}
+                                                    {{--<table class="table">--}}
+                                                        {{--<tbody>--}}
+                                                        {{--<tr>--}}
+                                                            {{--<th>Type</th>--}}
+                                                            {{--<td>{{substr($img->imageable_type, 11)}}</td>--}}
+                                                        {{--</tr>--}}
+                                                        {{--<tr>--}}
+                                                            {{--<th>{{substr($img->imageable_type, 11) . ' Id'}}</th>--}}
+                                                            {{--@if($img->imageable_type == 'App\Models\Product')--}}
+                                                                {{--<td>{{$img->product_id}}</td>--}}
+                                                            {{--@endif--}}
+                                                                {{--<td>{{$img->imageable_id}}</td>--}}
+                                                        {{--</tr>--}}
+                                                        {{--<tr>--}}
+                                                            {{--<th> URL</th>--}}
+                                                            {{--@if($img->imageable_type == 'App\Models\Product')--}}
+                                                                {{--<td>{{asset('cache/medium/'.$img->path)}}</td>--}}
+                                                            {{--@else--}}
+                                                                {{--<td> {{asset('/storage/' . $img->image)}}</td>--}}
+                                                            {{--@endif--}}
+                                                        {{--</tr>--}}
+                                                        {{--@if($img->imageable_type != 'App\Models\Product')--}}
+                                                        {{--<tr>--}}
+                                                            {{--<th> Created</th>--}}
+                                                            {{--<td> {{$img->created_at}}</td>--}}
+                                                        {{--</tr>--}}
+                                                        {{--@endif--}}
+                                                        {{--</tbody>--}}
+                                                    {{--</table>--}}
+                                                {{--</div>--}}
+                                            {{--</div>--}}
+                                            {{--<div class="modal-footer">--}}
+                                                {{--<button type="button" class="btn btn-info" data-dismiss="modal">Close</button>--}}
+                                            {{--</div>--}}
+                                        {{--</div>--}}
+                                    {{--</div>--}}
+                                {{--</div>--}}
+                            {{--@endforeach--}}
                     </div>
                 </div>
             </div>

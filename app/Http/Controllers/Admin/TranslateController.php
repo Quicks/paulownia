9fe7187c0ee3153a288cf26266894016fb725a97
s.stepaiko <stepaykosj@gmail.com>
2019-10-01 17:32:25 +0300

    Task#75_GoogleTranslate - GoogleTranslate_v2 prototype added

--- app/Http/Controllers/Admin/TranslateController.php
+++ app/Http/Controllers/Admin/TranslateController.php
@@ -4,8 +4,21 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Google\Cloud\Translate\TranslateClient;
 
-class \TranslateController extends Controller
+class TranslateController extends Controller
 {
-    //
+    public function translate(Request $request)
+    {
+        $translate = new TranslateClient([
+            'keyFile' => json_decode(file_get_contents(base_path(env('GOOGLE_APPLICATION_CREDENTIALS'))), true)
+        ]);
+
+        $result = $translate->translate($request->text, [
+                'source' => 'ru',
+                'target' => 'en'
+        ]);
+        
+        return $result['text'];
+    }
 }

b5bc2d7c0f9195cb35a32f3aa547917c300d510c
s.stepaiko <stepaykosj@gmail.com>
2019-10-21 21:30:00 +0300

    Started NewsPolitics task

--- config/acl.php
+++ config/acl.php
@@ -1,7 +1,157 @@
 <?php
 
 return [
-    
+    [
+        'key' => 'news',
+        'name' => 'News',
+        'route' => 'news.index',
+        'sort' => 1
+    ],
+    [
+        'key' => 'news.add',
+        'name' => 'Add',
+        'route' => 'news.create',
+        'sort' => 1
+    ], [
+        'key' => 'news.edit',
+        'name' => 'Edit',
+        'route' => 'news.edit',
+        'sort' => 2
+    ], [
+        'key' => 'news.delete',
+        'name' => 'Delete',
+        'route' => 'news.delete',
+        'sort' => 3
+    ],
+
+    [
+        'key' => 'articles',
+        'name' => 'Articles',
+        'route' => 'articles.index',
+        'sort' => 1
+    ],
+    [
+        'key' => 'articles.add',
+        'name' => 'Add',
+        'route' => 'articles.create',
+        'sort' => 1
+    ], [
+        'key' => 'articles.edit',
+        'name' => 'Edit',
+        'route' => 'articles.edit',
+        'sort' => 2
+    ], [
+        'key' => 'articles.delete',
+        'name' => 'Delete',
+        'route' => 'articles.delete',
+        'sort' => 3
+    ],
+
+    [
+        'key' => 'galleries',
+        'name' => 'Galleries',
+        'route' => 'galleries.index',
+        'sort' => 1
+    ],
+    [
+        'key' => 'galleries.add',
+        'name' => 'Add',
+        'route' => 'galleries.create',
+        'sort' => 1
+    ], [
+        'key' => 'galleries.edit',
+        'name' => 'Edit',
+        'route' => 'galleries.edit',
+        'sort' => 2
+    ], [
+        'key' => 'galleries.delete',
+        'name' => 'Delete',
+        'route' => 'galleries.delete',
+        'sort' => 3
+    ],
+
+    [
+        'key' => 'images',
+        'name' => 'Images',
+        'route' => 'images.index',
+        'sort' => 1
+    ],
+
+    [
+        'key' => 'treatises',
+        'name' => 'Treatises',
+        'route' => 'treatises.index',
+        'sort' => 1
+    ],
+    [
+        'key' => 'treatises.add',
+        'name' => 'Add',
+        'route' => 'treatises.create',
+        'sort' => 1
+    ], [
+        'key' => 'treatises.edit',
+        'name' => 'Edit',
+        'route' => 'treatises.edit',
+        'sort' => 2
+    ], [
+        'key' => 'treatises.delete',
+        'name' => 'Delete',
+        'route' => 'treatises.delete',
+        'sort' => 3
+    ],
+
+    [
+        'key' => 'offices',
+        'name' => 'Offices',
+        'route' => 'offices.index',
+        'sort' => 1
+    ],
+    [
+        'key' => 'offices.add',
+        'name' => 'Add',
+        'route' => 'offices.create',
+        'sort' => 1
+    ], [
+        'key' => 'offices.edit',
+        'name' => 'Edit',
+        'route' => 'offices.edit',
+        'sort' => 2
+    ], [
+        'key' => 'offices.delete',
+        'name' => 'Delete',
+        'route' => 'offices.delete',
+        'sort' => 3
+    ],
+
+    [
+        'key' => 'partners',
+        'name' => 'Partners',
+        'route' => 'partners.index',
+        'sort' => 1
+    ],
+    [
+        'key' => 'partners.add',
+        'name' => 'Add',
+        'route' => 'partners.create',
+        'sort' => 1
+    ], [
+        'key' => 'partners.edit',
+        'name' => 'Edit',
+        'route' => 'partners.edit',
+        'sort' => 2
+    ], [
+        'key' => 'partners.delete',
+        'name' => 'Delete',
+        'route' => 'partners.delete',
+        'sort' => 3
+    ],
+
+    [
+        'key' => 'tracker',
+        'name' => 'Tracking statistic',
+        'route' => 'tracker',
+        'sort' => 1
+    ],
 ];
 
 ?>
--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -6,95 +6,125 @@
                 <i class="fa fa-home fa-3x"></i><span>Welcome</span>
             </a>
         </li>
-        <li>
-            <a class="{{Request::is('admin/news*') ? "custom-admin-sidebar-a-active" : ""}}"
-               href="{{route('news.index')}}">
-                <i class="fa fa-newspaper-o fa-3x"></i><span>News</span>
-            </a>
-        </li>
-        <li>
-            <a class="{{Request::is('admin/articles*') ? "custom-admin-sidebar-a-active" : ""}}"
-               href="{{route('articles.index')}}">
-                <i class="fa fa-address-card-o fa-3x"></i><span>Articles</span>
-            </a>
-        </li>
-        <li>
-            <a class="{{Request::is('admin/galleries*') ? "custom-admin-sidebar-a-active" : ""}}"
-               href="{{route('galleries.index')}}">
-                <i class="fa fa-picture-o fa-3x"></i><span>Galleries</span>
-            </a>
-        </li>
-        <li>
-            <a class="{{Request::is('admin/images*') ? "custom-admin-sidebar-a-active" : ""}}"
-               href="{{route('images.index')}}">
-                <i class="fa fa-camera-retro fa-3x"></i><span>Images</span>
-            </a>
-        </li>
-        <li>
-            <a class="{{Request::is('admin/treatises*') ? "custom-admin-sidebar-a-active" : ""}}"
-               href="{{route('treatises.index')}}">
-                <i class="fa fa-book fa-3x"></i><span>Treatises</span>
-            </a>
-        </li>
-        <li>
-            <a class="{{Request::is('admin/offices*') ? "custom-admin-sidebar-a-active" : ""}}"
-               href="{{route('offices.index')}}">
-                <i class="fa fa-university fa-3x"></i><span>Official Offices</span>
-            </a>
-        </li>
-        <li>
-            <a class="{{Request::is('admin/partners*') ? "custom-admin-sidebar-a-active" : ""}}"
-               href="{{route('partners.index')}}">
-                <i class="fa fa-user-plus fa-3x"></i><span>Partners</span>
-            </a>
-        </li>
-        <li>
-            <a href="{{route('admin.dashboard.index')}}">
-                <i class="menu-properties icon dashboard-icon"></i>
-                    <span>Dashboard</span>
-            </a>
-        </li>
-        <li>
-            <a href="{{route('admin.sales.orders.index')}}">
-                <i class="menu-properties icon sales-icon"></i>
-                    <span>Sales</span>
-            </a>
-        </li>
-        <li>
-            <a href="{{route('admin.catalog.products.index')}}">
-                <i class="menu-properties icon catalog-icon"></i>
-                    <span>Catalog</span>
-            </a>
-        </li>
-        <li>
-            <a href="{{route('admin.customer.index')}}">
-                <i class="menu-properties icon customer-icon"></i>
-                <span>Customers</span>
-            </a>
-        </li>
-        <li>
-            <a href="{{route('admin.cart-rule.index')}}">
-                <i class="menu-properties icon promotions-icon"></i>
-                <span>Promotions</span>
-            </a>
-        </li>
-        <li>
-            <a href="{{route('admin.locales.index')}}">
-                <i class="menu-properties icon settings-icon"></i>
-                <span>Settings</span>
-            </a>
-        </li>
-        <li>
-            <a href="{{route('admin.configuration.index')}}">
-                <i class="menu-properties icon configuration-icon"></i>
-                <span>Configure</span>
-            </a>
-        </li>
-        <li>
-            <a class="{{Request::is('stats*') ? "custom-admin-sidebar-a-active" : ""}}"
-               href="{{route('tracker.stats.index')}}">
-                <i class="fa fa-line-chart fa-3x"></i><span>Tracking statistics</span>
-            </a>
-        </li>
+        @if(auth()->guard('admin')->user()->hasPermission('news'))
+            <li>
+                <a class="{{Request::is('admin/news*') ? "custom-admin-sidebar-a-active" : ""}}"
+                   href="{{route('news.index')}}">
+                    <i class="fa fa-newspaper-o fa-3x"></i><span>News</span>
+                </a>
+            </li>
+        @endif
+        @if(auth()->guard('admin')->user()->hasPermission('articles'))
+            <li>
+                <a class="{{Request::is('admin/articles*') ? "custom-admin-sidebar-a-active" : ""}}"
+                   href="{{route('articles.index')}}">
+                    <i class="fa fa-address-card-o fa-3x"></i><span>Articles</span>
+                </a>
+            </li>
+        @endif
+        @if(auth()->guard('admin')->user()->hasPermission('galleries'))
+            <li>
+                <a class="{{Request::is('admin/galleries*') ? "custom-admin-sidebar-a-active" : ""}}"
+                   href="{{route('galleries.index')}}">
+                    <i class="fa fa-picture-o fa-3x"></i><span>Galleries</span>
+                </a>
+            </li>
+        @endif
+        @if(auth()->guard('admin')->user()->hasPermission('images'))
+            <li>
+                <a class="{{Request::is('admin/images*') ? "custom-admin-sidebar-a-active" : ""}}"
+                   href="{{route('images.index')}}">
+                    <i class="fa fa-camera-retro fa-3x"></i><span>Images</span>
+                </a>
+            </li>
+        @endif
+        @if(auth()->guard('admin')->user()->hasPermission('treatises'))
+            <li>
+                <a class="{{Request::is('admin/treatises*') ? "custom-admin-sidebar-a-active" : ""}}"
+                   href="{{route('treatises.index')}}">
+                    <i class="fa fa-book fa-3x"></i><span>Treatises</span>
+                </a>
+            </li>
+        @endif
+        @if(auth()->guard('admin')->user()->hasPermission('offices'))
+            <li>
+                <a class="{{Request::is('admin/offices*') ? "custom-admin-sidebar-a-active" : ""}}"
+                   href="{{route('offices.index')}}">
+                    <i class="fa fa-university fa-3x"></i><span>Official Offices</span>
+                </a>
+            </li>
+        @endif
+        @if(auth()->guard('admin')->user()->hasPermission('partners'))
+            <li>
+                <a class="{{Request::is('admin/partners*') ? "custom-admin-sidebar-a-active" : ""}}"
+                   href="{{route('partners.index')}}">
+                    <i class="fa fa-user-plus fa-3x"></i><span>Partners</span>
+                </a>
+            </li>
+        @endif
+        @if(auth()->guard('admin')->user()->hasPermission('admin.dashboard'))
+            <li>
+                <a href="{{route('admin.dashboard.index')}}">
+                    <i class="menu-properties icon dashboard-icon"></i>
+                        <span>Dashboard</span>
+                </a>
+            </li>
+        @endif
+        @if(auth()->guard('admin')->user()->hasPermission('admin.sales.orders'))
+            <li>
+                <a href="{{route('admin.sales.orders.index')}}">
+                    <i class="menu-properties icon sales-icon"></i>
+                        <span>Sales</span>
+                </a>
+            </li>
+        @endif
+        @if(auth()->guard('admin')->user()->hasPermission('admin.catalog.products'))
+            <li>
+                <a href="{{route('admin.catalog.products.index')}}">
+                    <i class="menu-properties icon catalog-icon"></i>
+                        <span>Catalog</span>
+                </a>
+            </li>
+        @endif
+        @if(auth()->guard('admin')->user()->hasPermission('admin.customer'))
+            <li>
+                <a href="{{route('admin.customer.index')}}">
+                    <i class="menu-properties icon customer-icon"></i>
+                    <span>Customers</span>
+                </a>
+            </li>
+        @endif
+        @if(auth()->guard('admin')->user()->hasPermission('admin.cart-rule'))
+            <li>
+                <a href="{{route('admin.cart-rule.index')}}">
+                    <i class="menu-properties icon promotions-icon"></i>
+                    <span>Promotions</span>
+                </a>
+            </li>
+        @endif
+        @if(auth()->guard('admin')->user()->hasPermission('admin.locales'))
+            <li>
+                <a href="{{route('admin.locales.index')}}">
+                    <i class="menu-properties icon settings-icon"></i>
+                    <span>Settings</span>
+                </a>
+            </li>
+        @endif
+        @if(auth()->guard('admin')->user()->hasPermission('admin.configuration'))
+            <li>
+                <a href="{{route('admin.configuration.index')}}">
+                    <i class="menu-properties icon configuration-icon"></i>
+                    <span>Configure</span>
+                </a>
+            </li>
+        @endif
+        @if(auth()->guard('admin')->user()->hasPermission('tracker'))
+            <li>
+                <a class="{{Request::is('stats*') ? "custom-admin-sidebar-a-active" : ""}}"
+                   href="{{route('tracker.stats.index')}}">
+                    <i class="fa fa-line-chart fa-3x"></i><span>Tracking statistics</span>
+                </a>
+            </li>
+        @endif
     </ul>
 </div>

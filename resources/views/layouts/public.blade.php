579ce436719bb2e9ea4365a5f7326606f47f5a5f
Smirnova <smirnova270694@gmail.com>
2019-10-25 02:52:49 +0300

    added footer

---
+++ resources/views/layouts/public.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="{{ app()->getLocale() }}">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    {!! SEOMeta::generate() !!}
+    @if(empty(SEOMeta::getTitle()))
+        <title>{{ config('app.name', 'Laravel') }}</title>
+@endif
+
+<!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="{{ asset('css/public.css') }}">
+    @stack('css')
+</head>
+<body>
+<div id="app">
+    @if (session('flash_message'))
+        <div class="custom-status">
+            <span class="custom-icon"></span>
+            <p class="status-text"> {{ session('flash_message') }} </p>
+        </div>
+    @endif
+
+    <main>
+        @yield('content')
+    </main>
+</div>
+
+<!-- Scripts -->
+<script src="{{ asset('js/app.js') }}"></script>
+@stack('scripts')
+
+</body>
+</html>
---
+++ resources/views/public/main/footer.blade.php
@@ -0,0 +1,92 @@
+@push('css')
+    <link rel="stylesheet" href="{{ asset('css/footer.css') }}">
+@endpush
+<div class="footer">
+    <div class="footer-content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12">
+                    <div class="row">
+                        <div>
+                            <p class="title-text title-text-customers">
+                                To customers
+                            </p>
+                            <hr class="footer-line footer-line-1">
+                            <ul class="text-decoration text-decoration-customers">
+                                <li><a href="#">Stocks</a></li>
+                                <li><a href="#">Wholesale offers</a></li>
+                                <li><a href="#">Terms of sale</a></li>
+                                <li><a href="#">Terms of purchase</a></li>
+                                <li><a href="#">Terms of use</a></li>
+                            </ul>
+                        </div>
+                        <div>
+                            <p class="title-text title-text-menu">
+                                Menu
+                            </p>
+                            <hr class="footer-line footer-line-2">
+                            <ul class="text-decoration text-decoration-menu">
+                                <li><a href="#">Home</a></li>
+                                <li><a href="#">News</a></li>
+                                <li><a href="#">Goods</a></li>
+                                <li><a href="#">Profitability calculation</a></li>
+                                <li><a href="#">Gallery</a></li>
+                                <li><a href="#">FAQ</a></li>
+                                <li><a href="#">About Us</a></li>
+                            </ul>
+                        </div>
+                        <div>
+                            <p class="title-text title-text-paulownia">
+                                Paulownia
+                            </p>
+                            <hr class="footer-line footer-line-3">
+                            <ul class="text-decoration text-decoration-paulownia">
+                                <li><a href="#">Paulownia Shan Tong</a></li>
+                                <li><a href="#">Paulownia Elongata</a></li>
+                                <li><a href="#">Paulownia Tomentosa</a></li>
+                                <li><a href="#">Paulownia Kawakamii</a></li>
+                                <li><a href="#">Paulownia Turbo Pro</a></li>
+                                <li><a href="#">Paulownia Ze Pro</a></li>
+                            </ul>
+                        </div>
+                        <div>
+                            <p class="title-text title-text-contacts">
+                                Contacts
+                            </p>
+                            <hr class="footer-line footer-line-4">
+                            <ul class="text-decoration text-decoration-contacts">
+                                <li>Phone: +34 642 787 555</li>
+                                <li>Email: info@paulownia.pro</li>
+                                <li>Viber: +34 642 787 555</li>
+                                <li>We work:</li>
+                                <li>Mon.-Fri. from 09:00 before 18:00</li>
+                            </ul>
+                            <a href="#" class="networks linkedin"><img src="../../../images/linkedin.svg"></a>
+                            <a href="#" class="networks facebook"><img src="../../../images/fb.svg"></a>
+                            <a href="#" class="networks instagram"><img src="../../../images/instagram.svg"></a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<div class="footer-bottom">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col">
+                <div class="footer-bottom-copy">
+                    Copyright © by Paulownia.pro® 2019
+                </div>
+            </div>
+            <div class="col">
+                <div class="footer-bottom-booklet">
+                    <a href="#">
+                        <img src="../../../images/download-booklet.svg">
+                        <span> booklet of Paulownia</span>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>

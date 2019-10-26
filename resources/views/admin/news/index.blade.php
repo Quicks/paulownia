d509ec720a6ff4dbac3a01bff1d154c2271146f8
s.stepaiko <stepaykosj@gmail.com>
2019-10-26 13:55:25 +0300

    Task#125 - done

--- resources/views/admin/news/index.blade.php
+++ resources/views/admin/news/index.blade.php
@@ -40,20 +40,24 @@
                                         <td>{{ $item->name }}</td><td>{{ $item->active }}</td><td>{{ $item->publish_date }}</td>
                                         <td>
                                             <a href="{{ url('/admin/news/' . $item->id) }}" title="View News"><button class="btn btn-info btn-sm"><i class="fa fa-eye" aria-hidden="true"></i> View</button></a>
-                                            <a href="{{ url('/admin/news/' . $item->id . '/edit') }}" title="Edit News"><button class="btn btn-primary btn-sm"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</button></a>
-                                            <a href="{{ url('/admin/image_add/?imageable_id=' . $item->id . '&imageable_type=' . get_class($item) . '&redirect_route='.route('news.show', $item->id) )  }}"
-                                               title="Add Image">
-                                                <button class="btn btn-primary btn-sm">
-                                                    <i class="fa fa-picture-o" aria-hidden="true"></i>
-                                                    Add image
-                                                </button>
-                                            </a>
 
-                                            <form method="POST" action="{{ url('/admin/news' . '/' . $item->id) }}" accept-charset="UTF-8" style="display:inline">
-                                                {{ method_field('DELETE') }}
-                                                {{ csrf_field() }}
-                                                <button type="submit" class="btn btn-danger btn-sm" title="Delete News" onclick="return confirm(&quot;Confirm delete?&quot;)"><i class="fa fa-trash-o" aria-hidden="true"></i> Delete</button>
-                                            </form>
+                                            @if(bouncer()->hasPermission('news.edit'))
+                                                <a href="{{ url('/admin/news/' . $item->id . '/edit') }}" title="Edit News"><button class="btn btn-primary btn-sm"><i class="fa fa-pencil-square-o" aria-hidden="true"></i> Edit</button></a>
+                                                <a href="{{ url('/admin/image_add/?imageable_id=' . $item->id . '&imageable_type=' . get_class($item) . '&redirect_route='.route('news.show', $item->id) )  }}"
+                                                   title="Add Image">
+                                                    <button class="btn btn-primary btn-sm">
+                                                        <i class="fa fa-picture-o" aria-hidden="true"></i>
+                                                        Add image
+                                                    </button>
+                                                </a>
+                                            @endif
+                                            @if(bouncer()->hasPermission('news.destroy'))
+                                                <form method="POST" action="{{ url('/admin/news' . '/' . $item->id) }}" accept-charset="UTF-8" style="display:inline">
+                                                    {{ method_field('DELETE') }}
+                                                    {{ csrf_field() }}
+                                                    <button type="submit" class="btn btn-danger btn-sm" title="Delete News" onclick="return confirm(&quot;Confirm delete?&quot;)"><i class="fa fa-trash-o" aria-hidden="true"></i> Delete</button>
+                                                </form>
+                                            @endif
                                         </td>
                                     </tr>
                                 @endforeach

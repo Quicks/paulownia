abb3d95e3c51e39bc218ccf971694e1303c3e1cc
Orenchak <vitaorenchak@gmail.com>
2019-10-31 10:50:12 +0200

    Task117

---
+++ resources/views/public/main/main-slider.blade.php
@@ -0,0 +1,53 @@
+@push('css')
+    <link rel="stylesheet" href="{{asset('css/actions.css') }}">
+@endpush
+
+<div class="mainAction">
+    <div class="row">
+        <div class="col">
+            <div class="action">
+                <div class="discountInPercent">-3%</div>
+                <div class="discountText">
+                    Praesent cursus magna, vel scelerisque nisl consectetur.</div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="action">
+                <div class="discountInPercent">-40%</div>
+                <div class="discountText">
+                    Praesent  cursus magna, vel scelerisque nisl consectetur.</div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="action">
+                <div class="discountInPercent">-3% </div>
+                <div class="discountText">Praesent  cursus magna, vel scelerisque nisl consectetur.</div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="action">
+                <div class="discountInPercent">-40% </div>
+                <div class="discountText">Praesent  cursus magna, vel scelerisque nisl consectetur.</div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="action">
+                <div class="discountInPercent">-3% </div>
+                <div class="discountText">Praesent cursus magna, vel scelerisque nisl consectetur.</div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="action">
+                <div class="discountInPercent">-3%</div>
+                <div class="discountText">Praesent cursus magna, vel scelerisque nisl consectetur.</div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="our-services">
+
+
+</div>
+
+

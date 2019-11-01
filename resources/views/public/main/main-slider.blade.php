ce8d72f0b1e2a9ecf9a8b4c570a08eadc8aa0b74
Orenchak <vitaorenchak@gmail.com>
2019-11-01 18:52:51 +0200

    changes in files

--- resources/views/public/main/main-slider.blade.php
+++ resources/views/public/main/main-slider.blade.php
@@ -92,18 +92,22 @@
 </div>
 
 
-    {{--<div class="main-title">Our services</div>--}}
-        {{--<div class="d-flex justify-content-center">--}}
-            {{--<div class="main-container">--}}
-                {{--<img class="img-left" src="images/our-services-1.jpg" alt="image" width="536px" height="318px">--}}
-                {{--<span class="main-text-container">--}}
-                {{--<div class="title-right">Sale of seedlings and paulownia trees</div>--}}
-                {{--<div class="text-right"> Sale of seedlings and paulownia trees, Sale of seedlings and paulownia trees--}}
-                    {{--Sale of seedlings and paulownia trees Sale of seedlings and paulownia trees...</div>--}}
-                {{--<a href="#">Read more</a>--}}
-                {{--</span>--}}
-            {{--</div>--}}
-        {{--</div>--}}
+    <div class="main-title">Our services</div>
+        <div class="d-flex justify-content-center">
+            <div class="row">
+                <div class="col">
+            <div class="main-container">
+                <img class="img-left" src="images/our-services-1.jpg" alt="image" width="536px" height="318px">
+                <span class="main-text-container">
+                <div class="title-right">Sale of seedlings and paulownia trees</div>
+                <div class="text-position-right"> Sale of seedlings and paulownia trees, Sale of seedlings and paulownia trees
+                    Sale of seedlings and paulownia trees Sale of seedlings and paulownia trees...
+                <a class="read-more "href="#">Read more</a></div>
+                </span>
+            </div>
+        </div>
+            </div>
+        </div>
 
 
 
--- resources/views/public/main/main.blade.php
+++ resources/views/public/main/main.blade.php
@@ -1,5 +1,6 @@
 @extends('layouts.public')
 @section('content')
     @include('public.main.main-slider')
+    <div style="height: 2200px;"></div>
     @include('public.main.advantages')
 @endsection

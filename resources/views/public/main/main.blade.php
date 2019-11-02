a2422401f306fe5cbec78a979297a807dfa8bd31
Orenchak <vitaorenchak@gmail.com>
2019-11-02 14:08:24 +0200

    rename files

--- resources/views/public/main/main.blade.php
+++ resources/views/public/main/main.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.public')
 @section('content')
-    @include('public.main.main-slider')
-    @include('public.main.main-actions')
+    @include('public.main.slider')
+    @include('public.main.actions')
     <div style="height: 2200px"></div>
     @include('public.main.advantages')
 @endsection

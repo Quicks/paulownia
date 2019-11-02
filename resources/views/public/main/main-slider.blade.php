4687857f98fd733cb26c7cbd0d7cfe4d4f1727ee
Orenchak <vitaorenchak@gmail.com>
2019-11-02 12:50:58 +0200

    changes in files

--- resources/views/public/main/main-slider.blade.php
+++ resources/views/public/main/main-slider.blade.php
@@ -48,66 +48,66 @@
     </div>
 </div>
 
-<div class="mainAction">
-    <div class="row">
-        <div class="col">
-            <div class="action">
-                <div class="discountInPercent">-3%</div>
-                <div class="discountText">
-                    Praesent cursus magna, vel scelerisque nisl consectetur.</div>
-            </div>
-        </div>
-        <div class="col">
-            <div class="action">
-                <div class="discountInPercent">-40%</div>
-                <div class="discountText">
-                    Praesent  cursus magna, vel scelerisque nisl consectetur.</div>
-            </div>
-        </div>
-        <div class="col">
-            <div class="action">
-                <div class="discountInPercent">-3% </div>
-                <div class="discountText">Praesent  cursus magna, vel scelerisque nisl consectetur.</div>
-            </div>
-        </div>
-        <div class="col">
-            <div class="action">
-                <div class="discountInPercent">-40% </div>
-                <div class="discountText">Praesent  cursus magna, vel scelerisque nisl consectetur.</div>
-            </div>
-        </div>
-        <div class="col">
-            <div class="action">
-                <div class="discountInPercent">-3% </div>
-                <div class="discountText">Praesent cursus magna, vel scelerisque nisl consectetur.</div>
-            </div>
-        </div>
-        <div class="col">
-            <div class="action">
-                <div class="discountInPercent">-3%</div>
-                <div class="discountText">Praesent cursus magna, vel scelerisque nisl consectetur.</div>
-            </div>
-        </div>
-    </div>
-</div>
+{{--<div class="mainAction">--}}
+    {{--<div class="row">--}}
+        {{--<div class="col">--}}
+            {{--<div class="action">--}}
+                {{--<div class="discountInPercent">-3%</div>--}}
+                {{--<div class="discountText">--}}
+                    {{--Praesent cursus magna, vel scelerisque nisl consectetur.</div>--}}
+            {{--</div>--}}
+        {{--</div>--}}
+        {{--<div class="col">--}}
+            {{--<div class="action">--}}
+                {{--<div class="discountInPercent">-40%</div>--}}
+                {{--<div class="discountText">--}}
+                    {{--Praesent  cursus magna, vel scelerisque nisl consectetur.</div>--}}
+            {{--</div>--}}
+        {{--</div>--}}
+        {{--<div class="col">--}}
+            {{--<div class="action">--}}
+                {{--<div class="discountInPercent">-3% </div>--}}
+                {{--<div class="discountText">Praesent  cursus magna, vel scelerisque nisl consectetur.</div>--}}
+            {{--</div>--}}
+        {{--</div>--}}
+        {{--<div class="col">--}}
+            {{--<div class="action">--}}
+                {{--<div class="discountInPercent">-40% </div>--}}
+                {{--<div class="discountText">Praesent  cursus magna, vel scelerisque nisl consectetur.</div>--}}
+            {{--</div>--}}
+        {{--</div>--}}
+        {{--<div class="col">--}}
+            {{--<div class="action">--}}
+                {{--<div class="discountInPercent">-3% </div>--}}
+                {{--<div class="discountText">Praesent cursus magna, vel scelerisque nisl consectetur.</div>--}}
+            {{--</div>--}}
+        {{--</div>--}}
+        {{--<div class="col">--}}
+            {{--<div class="action">--}}
+                {{--<div class="discountInPercent">-3%</div>--}}
+                {{--<div class="discountText">Praesent cursus magna, vel scelerisque nisl consectetur.</div>--}}
+            {{--</div>--}}
+        {{--</div>--}}
+    {{--</div>--}}
+{{--</div>--}}
 
 
-    <div class="main-title">Our services</div>
-        <div class="d-flex justify-content-center">
-            <div class="row">
-                <div class="col">
-                     <div class="main-container">
-                         <img class="img-left" src="images/our-services-1.jpg" alt="image" width="536px" height="318px">
-                             <span class="main-text-container">
-                                 <div class="title-right">Sale of seedlings and paulownia trees</div>
-                                    <div class="text-position-right"> Sale of seedlings and paulownia trees, Sale of seedlings and paulownia trees
-                                                                        Sale of seedlings and paulownia trees Sale of seedlings and paulownia trees...
-                                        <a class="read-more "href="#">Read more</a></div>
-                             </span>
-                     </div>
-                </div>
-            </div>
-        </div>
+    {{--<div class="main-title">Our services</div>--}}
+        {{--<div class="d-flex justify-content-center">--}}
+            {{--<div class="row">--}}
+                {{--<div class="col">--}}
+                     {{--<div class="main-container">--}}
+                         {{--<img class="img-left" src="images/our-services-1.jpg" alt="image" width="536px" height="318px">--}}
+                             {{--<span class="main-text-container">--}}
+                                 {{--<div class="title-right">Sale of seedlings and paulownia trees</div>--}}
+                                    {{--<div class="text-position-right"> Sale of seedlings and paulownia trees, Sale of seedlings and paulownia trees--}}
+                                                                        {{--Sale of seedlings and paulownia trees Sale of seedlings and paulownia trees...--}}
+                                        {{--<a class="read-more "href="#">Read more</a></div>--}}
+                             {{--</span>--}}
+                     {{--</div>--}}
+                {{--</div>--}}
+            {{--</div>--}}
+        {{--</div>--}}
 
 
 
--- resources/views/public/main/main.blade.php
+++ resources/views/public/main/main.blade.php
@@ -1,6 +1,8 @@
 @extends('layouts.public')
 @section('content')
     @include('public.main.main-slider')
+    @include('public.main.main-actions')
+    @include('public.main.our-services')
     <div style="height: 2200px;"></div>
     @include('public.main.advantages')
 @endsection

0d3b0f9c610d061b26ad2762c3e99f1d0d85e904
Orenchak <vitaorenchak@gmail.com>
2019-11-02 14:04:26 +0200

    create interface

--- resources/views/public/main/our-services.blade.php
+++ resources/views/public/main/our-services.blade.php
@@ -1,19 +1,81 @@
 <link rel="stylesheet" href="{{asset('css/our-services.css') }}">
 
 
-<div class="main-title">Our services</div>
-<div class="d-flex justify-content-center">
-    <div class="row">
-        <div class="col">
-            <div class="main-container">
-                <img class="img-left" src="images/our-services-1.jpg" alt="image" width="536px" height="318px">
-                <span class="main-text-container">
+    <div class="main-title">Our services</div>
+
+    <div class="container-width">
+        <div class="d-flex justify-content-center">
+            <div class="row">
+                <div class="col">
+                    <div class="main-container">
+                         <img class="img-left" src="images/our-services-1.jpg" alt="image" width="536px" height="318px">
+                            <span class="main-text-container-right">
                                  <div class="title-right">Sale of seedlings and paulownia trees</div>
                                     <div class="text-position-right"> Sale of seedlings and paulownia trees, Sale of seedlings and paulownia trees
                                                                         Sale of seedlings and paulownia trees Sale of seedlings and paulownia trees...
-                                        <a class="read-more "href="#">Read more</a></div>
-                             </span>
+                                        <a class="read-more-right"href="#">Read more</a>
+                                    </div>
+                            </span>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
-</div>
+
+    <div class="container-width">
+        <div class="d-flex justify-content-center">
+            <div class="row">
+                <div class="col">
+                    <div class="main-container">
+                        <img class="img-right" src="images/our-services-2.jpg" alt="image" width="536px" height="318px">
+                        <span class="main-text-container-left">
+                                 <div class="title-left">Sale of seedlings and paulownia trees</div>
+                                    <div class="text-position-left"> Sale of seedlings and paulownia trees, Sale of seedlings and paulownia trees
+                                                                        Sale of seedlings and paulownia trees Sale of seedlings and paulownia trees...
+                                        <a class="read-more-left "href="#">Read more</a>
+                                    </div>
+                            </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container-width">
+        <div class="d-flex justify-content-center">
+            <div class="row">
+                <div class="col">
+                    <div class="main-container">
+                        <img class="img-left" src="images/our-services-3.jpg" alt="image" width="536px" height="318px">
+                        <span class="main-text-container-right">
+                                         <div class="title-right">Sale of seedlings and paulownia trees</div>
+                                            <div class="text-position-right"> Sale of seedlings and paulownia trees, Sale of seedlings and paulownia trees
+                                                                                Sale of seedlings and paulownia trees Sale of seedlings and paulownia trees...
+                                                <a class="read-more-right"href="#">Read more</a>
+                                            </div>
+                                    </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="container-width">
+        <div class="d-flex justify-content-center">
+            <div class="row">
+                <div class="col">
+                    <div class="main-container">
+                        <img class="img-right" src="images/our-services-4.jpg" alt="image" width="536px" height="318px">
+                        <span class="main-text-container-left">
+                                         <div class="title-left">Sale of seedlings and paulownia trees</div>
+                                            <div class="text-position-left"> Sale of seedlings and paulownia trees, Sale of seedlings and paulownia trees
+                                                                                Sale of seedlings and paulownia trees Sale of seedlings and paulownia trees...
+                                                <a class="read-more-left "href="#">Read more</a>
+                                            </div>
+                                    </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+

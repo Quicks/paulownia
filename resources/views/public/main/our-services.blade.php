c263d34f5ba02d54130ce88bc29cd5920abfb4f8
Orenchak <vitaorenchak@gmail.com>
2019-11-02 14:17:46 +0200

    changes in image and text

--- resources/views/public/main/our-services.blade.php
+++ resources/views/public/main/our-services.blade.php
@@ -29,9 +29,9 @@
                     <div class="main-container">
                         <img class="img-right" src="images/our-services-2.jpg" alt="image" width="536px" height="318px">
                         <span class="main-text-container-left">
-                                 <div class="title-left">Sale of seedlings and paulownia trees</div>
-                                    <div class="text-position-left"> Sale of seedlings and paulownia trees, Sale of seedlings and paulownia trees
-                                                                        Sale of seedlings and paulownia trees Sale of seedlings and paulownia trees...
+                                 <div class="title-left">Analysis and personal design future plantations</div>
+                                    <div class="text-position-left">  Analysis and personal design of future plantations
+                                        Analysis and personal design of future plantations...
                                         <a class="read-more-left "href="#">Read more</a>
                                     </div>
                             </span>
@@ -48,9 +48,9 @@
                     <div class="main-container">
                         <img class="img-left" src="images/our-services-3.jpg" alt="image" width="536px" height="318px">
                         <span class="main-text-container-right">
-                                         <div class="title-right">Sale of seedlings and paulownia trees</div>
-                                            <div class="text-position-right"> Sale of seedlings and paulownia trees, Sale of seedlings and paulownia trees
-                                                                                Sale of seedlings and paulownia trees Sale of seedlings and paulownia trees...
+                                         <div class="title-right">ROI calculation </div>
+                                            <div class="text-position-right"> Calculation of return on investment Calculation of return on investment
+                                                Calculation of return on investment Calculation of return on investment...
                                                 <a class="read-more-right"href="#">Read more</a>
                                             </div>
                                     </span>
@@ -67,9 +67,9 @@
                     <div class="main-container">
                         <img class="img-right" src="images/our-services-4.jpg" alt="image" width="536px" height="318px">
                         <span class="main-text-container-left">
-                                         <div class="title-left">Sale of seedlings and paulownia trees</div>
-                                            <div class="text-position-left"> Sale of seedlings and paulownia trees, Sale of seedlings and paulownia trees
-                                                                                Sale of seedlings and paulownia trees Sale of seedlings and paulownia trees...
+                                         <div class="title-left">Consultation during the cultivation</div>
+                                            <div class="text-position-left"> Consultation during the cultivation Consultation during the cultivation
+                                                Consultation during the cultivation...
                                                 <a class="read-more-left "href="#">Read more</a>
                                             </div>
                                     </span>

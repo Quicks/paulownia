a7f8dc049abb695bcf782c8e5e5fbad634570f50
Smirnova <smirnova270694@gmail.com>
2019-11-07 17:22:32 +0200

    fixed controller

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Webkul\Shop\Http\Controllers\Controller;
 use Webkul\Core\Repositories\SliderRepository;
 
 class MainController extends Controller

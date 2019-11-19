3f5c5a0a21609db4aa6ad5eb7fed1cc72cc8d43a
Smirnova <smirnova270694@gmail.com>
2019-11-19 14:45:09 +0200

    changed footer

--- resources/views/public/footer.blade.php
+++ resources/views/public/footer.blade.php
@@ -1,6 +1,16 @@
 <div class="footer py-4">
     <div class="footer-content row mx-auto">
-        <div class="col-xl-3 col-md-6 col-sm-12">
+        <div class="col-xl-2 col-md-6 col-sm-12">
+            <p class="footer-title">  Our services </p>
+            <hr class="footer-line mt-n3">
+            <ul class="text-list m-0 p-0">
+                <li><a href="#">Sale of seedlings </a></li>
+                <li><a href="#">Analysis and personal design</a></li>
+                <li><a href="#">ROI calculation</a></li>
+                <li><a href="#">Consultation during the cultivation</a></li>
+            </ul>
+        </div>
+        <div class="col-xl-2 col-md-6 col-sm-12">
             <p class="footer-title">@lang('header-footer.to customers') </p>
             <hr class="footer-line mt-n3">
             <ul class="text-list m-0 p-0">
@@ -11,29 +21,18 @@
                 <li><a href="#">@lang('header-footer.terms of use')</a></li>
             </ul>
         </div>
-        <div class="col-xl-3 col-md-6 col-sm-12">
+        <div class="col-xl-2 col-md-6 col-sm-12">
             <p class="footer-title">@lang('header-footer.menu') </p>
             <hr class="footer-line mt-n3">
             <ul class="text-list m-0 p-0">
                 <li><a href="{{route('main')}}">@lang('header-footer.main')</a></li>
                 <li><a href="{{route('public.news.index')}}">@lang('header-footer.news')</a></li>
+                <li><a href="{{route('public.paulownia.index')}}">Paulownia</a></li>
                 <li><a href="{{route('public.products.index')}}">@lang('header-footer.goods')</a></li>
                 <li><a href="{{route('public.calculations.index')}}">@lang('header-footer.profitability calculation')</a></li>
                 <li><a href="{{route('public.galleries.index')}}">@lang('header-footer.gallery')</a></li>
                 <li><a href="{{route('public.faq.index')}}">@lang('header-footer.faq')</a></li>
                 <li><a href="{{route('public.about-us.index')}}">@lang('header-footer.about us')</a></li>
-            </ul>
-        </div>
-        <div class="col-xl-3 col-md-6 col-sm-12">
-            <p class="footer-title">  @lang('header-footer.paulownia') </p>
-            <hr class="footer-line mt-n3">
-            <ul class="text-list m-0 p-0">
-                <li><a href="#">@lang('header-footer.paulownia shan tong') </a></li>
-                <li><a href="#">@lang('header-footer.paulownia elongata')</a></li>
-                <li><a href="#">@lang('header-footer.paulownia tomentosa')</a></li>
-                <li><a href="#">@lang('header-footer.paulownia kawakamii')</a></li>
-                <li><a href="#">@lang('header-footer.paulownia turbo pro')</a></li>
-                <li><a href="#">@lang('header-footer.paulownia ze pro')</a></li>
             </ul>
         </div>
         <div class="col-xl-3 col-md-6 col-sm-12">
@@ -54,15 +53,18 @@
                 </ul>
             </ul>
         </div>
+        <div class="col-xl-3 col-md-6 col-sm-12">
+            <div>
+                <img class="mb-4" src="{{asset('images/booklet_en.png')}}">
+            </div>
+            <div>
+                <a href="#"><img width="310px" src="{{asset('images/download-booklet.svg')}}"></a>
+            </div>
+        </div>
     </div>
 </div>
 <div class="row footer-bottom text-center">
-    <div class="col-xl-6 col-md-6 col-sm-12 footer-bottom-copy py-5">
+    <div class="col-12 footer-bottom-copy py-5">
         <span>Copyright © by Paulownia.pro® 2019</span>
-    </div>
-    <div class="col-xl-6 col-md-6 col-sm-12 py-4">
-        <a href="#">
-            <img src="{{asset('images/download-booklet.svg')}}">
-        </a>
     </div>
 </div>

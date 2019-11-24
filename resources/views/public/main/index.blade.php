13c321d3eb2b07389cc3541bd93aecc48429255d
Orenchak <vitaorenchak@gmail.com>
2019-11-24 23:43:58 +0200

    about

--- resources/views/public/main/index.blade.php
+++ resources/views/public/main/index.blade.php
@@ -2,6 +2,7 @@
 @section('content')
     @include('public.main.slider')
     @include('public.main.actions')
+    @include('public.main.about-us')
     <div style="height: 2200px"></div>
     @include('public.main.advantages')
     @include('public.main.varieties')

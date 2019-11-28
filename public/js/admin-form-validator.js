c5e16f0fe2e21a9eba4f901ff340bcd0531b3186
Smirnova <smirnova270694@gmail.com>
2019-11-28 19:21:57 +0200

    added services

--- public/js/admin-form-validator.js
+++ public/js/admin-form-validator.js
@@ -1,12 +1,12 @@
 $(document).ready(function () {
     $("input[type='submit']").on('click', function () {
         let form = $('.validForm');
-        if(!form[0].checkValidity()){
+        if (!form[0].checkValidity()) {
             $('#main-form-btn').click();
         }
     });
     $('.validForm').on('submit', function (event) {
-        if(!document.validForm()) {
+        if (!document.validForm()) {
             event.preventDefault();
             alert('All fields must be filled in at least one language!');
         }
@@ -15,7 +15,7 @@ $(document).ready(function () {
     document.validForm = function validForm() {
         var isOneLanguagefilled = false;
         $.each(allLangArr, function (index, value) {
-            var empty = $('[name^='+ value +']').filter(function () {
+            var empty = $('[name^=' + value + ']').filter(function () {
                 return $(this).val().trim() === "";
             });
             if (!empty.length) {
---
+++ resources/views/public/main/services.blade.php
@@ -0,0 +1,56 @@
+@push('css')
+    <link rel="stylesheet" href="{{asset('css/services.css') }}">
+@endpush
+
+<div class="our-services">
+    <img src="/images/service-wave-up.png" width="100%" style="margin-top: -70px">
+    <div class="row mx-auto" style="max-width: 1800px">
+        <div class="col-12 mt-5">
+            <p class="services-title text-center">Our services</p>
+            <hr class="services-line">
+        </div>
+        <div class="col-xl-3 col-md-6 col-sm-12 services-1 mx-auto services-item-width mb-sm-2">
+            <div class="mx-auto" style="max-width: 190px">
+                <p class="services-item-title">Sale of seedlings and paulownia trees</p>
+            </div>
+            <div class="mx-auto services-item-text-width">
+                <p class="services-item-text">Sale of seedlings and paulownia trees, Sale of seedlings and paulownia trees
+                    Sale of seedlings and paulownia trees...</p>
+                <a class="services-item-link" href="#" target="_blank">Read more</a>
+            </div>
+        </div>
+        <div class="col-xl-3 col-md-6 col-sm-12 services-2 mx-auto services-item-width mb-sm-2">
+            <div class="mx-auto" style="max-width: 190px">
+                <p class="services-item-title">Analysis and personal design future plantations</p>
+            </div>
+            <div class="mx-auto services-item-text-width">
+                <p class="services-item-text">Analysis and personal design of future plantations
+                    Analysis and personal design of future plantations...</p>
+                <a class="services-item-link" href="#" target="_blank">Read more</a>
+            </div>
+        </div>
+        <div class="col-xl-3 col-md-6 col-sm-12 services-3 mx-auto services-item-width mb-sm-2">
+            <div class="mx-auto pt-4" style="max-width: 190px">
+                <p class="services-item-title">ROI calculation</p>
+            </div>
+            <div class="mx-auto services-item-text-width pt-3">
+                <p class="services-item-text">Calculation of return on investment Calculation of return on investment
+                    Calculation of return on...</p>
+                <a class="services-item-link" href="#" target="_blank">Read more</a>
+            </div>
+        </div>
+        <div class="col-xl-3 col-md-6 col-sm-12 services-4 mx-auto services-item-width mb-sm-2">
+            <div class="mx-auto" style="max-width: 190px">
+                <p class="services-item-title">Consultation during the cultivation</p>
+            </div>
+            <div class="mx-auto services-item-text-width pt-2">
+                <p class="services-item-text">Consultation during the cultivation Consultation during the cultivation
+                    Consultation during the...</p>
+                <a class="services-item-link" href="#" target="_blank">Read more</a>
+            </div>
+        </div>
+    </div>
+
+
+</div>
+<img src="/images/service-wave-down.png" width="100%">

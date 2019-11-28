74f5a49f3278caa268ecc3eba7fcc4fbc937ec27
Orenchak <vitaorenchak@gmail.com>
2019-11-28 19:15:16 +0200

    image background

--- resources/views/public/main/index.blade.php
+++ resources/views/public/main/index.blade.php
@@ -2,8 +2,10 @@
 @section('content')
     @include('public.main.slider')
     @include('public.main.slider-sale')
+
     @include('public.main.about-us')
-    {{-- <div style="height: 2200px"></div> --}}
+
+     {{--<div style="height: 2200px"></div> --}}
     @include('public.main.advantages')
     @include('public.main.varieties')
     @include('public.main.calculate')
---
+++ resources/views/public/main/vertical-slider.blade.php
@@ -0,0 +1,115 @@
+{{--@push('css')--}}
+    {{--<link rel="stylesheet" href="{{ asset('css/stick.css') }}">--}}
+    {{--<link rel="stylesheet" href="{{ asset('css/stick-theme.css') }}">--}}
+{{--@endpush--}}
+
+
+{{--<style type="text/css">--}}
+
+    {{--.slider {--}}
+        {{--width: 50%;--}}
+        {{--margin: 100px auto;--}}
+    {{--}--}}
+
+    {{--.slick-slide {--}}
+        {{--margin: 0px 20px;--}}
+    {{--}--}}
+
+    {{--.slick-slide img {--}}
+        {{--width: 100%;--}}
+    {{--}--}}
+
+    {{--.slick-prev:before,--}}
+    {{--.slick-next:before {--}}
+        {{--color: black;--}}
+    {{--}--}}
+
+
+    {{--.slick-slide {--}}
+        {{--transition: all ease-in-out .3s;--}}
+        {{--opacity: .2;--}}
+    {{--}--}}
+
+    {{--.slick-active {--}}
+        {{--opacity: .5;--}}
+    {{--}--}}
+
+    {{--.slick-current {--}}
+        {{--opacity: 1;--}}
+    {{--}--}}
+{{--</style>--}}
+
+
+{{--<body>  <div style="width: 345px;height: 556px;background:rosybrown">--}}
+    {{--<section class="vertical slider">--}}
+        {{--<div>--}}
+            {{--<img src="http://placehold.it/350x100?text=1">--}}
+            {{--<p>khkshgkhgkxhg</p>--}}
+            {{--<p>khkshgkhgkxhg</p>--}}
+
+        {{--</div>--}}
+        {{--<div>--}}
+            {{--<img src="http://placehold.it/350x100?text=2">--}}
+            {{--<p>khkshgkhgkxhg</p>--}}
+            {{--<p>khkshgkhgkxhg</p>--}}
+        {{--</div>--}}
+        {{--<div>--}}
+            {{--<img src="http://placehold.it/350x100?text=3">--}}
+            {{--<p>khkshgkhgkxhg</p>--}}
+            {{--<p>khkshgkhgkxhg</p>--}}
+        {{--</div>--}}
+        {{--<div>--}}
+            {{--<img src="http://placehold.it/350x100?text=4">--}}
+            {{--<p>khkshgkhgkxhg</p>--}}
+            {{--<p>khkshgkhgkxhg</p>--}}
+        {{--</div>--}}
+        {{--<div>--}}
+            {{--<img src="http://placehold.it/350x100?text=5">--}}
+            {{--<p>khkshgkhgkxhg</p>--}}
+            {{--<p>khkshgkhgkxhg</p>--}}
+        {{--</div>--}}
+        {{--<div>--}}
+            {{--<img src="http://placehold.it/350x100?text=6">--}}
+        {{--</div>--}}
+        {{--<div>--}}
+            {{--<img src="http://placehold.it/350x100?text=7">--}}
+        {{--</div>--}}
+        {{--<div>--}}
+            {{--<img src="http://placehold.it/350x100?text=8">--}}
+        {{--</div>--}}
+        {{--<div>--}}
+            {{--<img src="http://placehold.it/350x100?text=9">--}}
+        {{--</div>--}}
+        {{--<div>--}}
+            {{--<img src="http://placehold.it/350x100?text=10">--}}
+        {{--</div>--}}
+    {{--</section>--}}
+{{--</div>--}}
+
+
+
+
+
+
+{{--@push('scripts')--}}
+
+    {{--<script src="{{ asset('js/slick.min.js') }}"></script>--}}
+{{--<script src="{{ asset('js/slick.js') }}"></script>--}}
+
+
+{{--<script type="text/javascript">--}}
+        {{--$(document).ready(function () {--}}
+            {{--$(document).on('ready', function () {--}}
+                {{--$(".vertical").slick({--}}
+
+                    {{--vertical: true,--}}
+                    {{--slidesToShow: 3,--}}
+                    {{--slidesToScroll: 1,--}}
+                    {{--autoplay: true--}}
+                {{--});--}}
+            {{--});--}}
+        {{--});--}}
+    {{--</script>--}}
+
+
+{{--@endpush--}}

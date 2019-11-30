59663ede161a43ae27cd54373c45c2c81e394dc1
Smirnova <smirnova270694@gmail.com>
2019-11-30 13:52:01 +0200

    added our services

--- resources/views/public/main/services.blade.php
+++ resources/views/public/main/services.blade.php
@@ -2,10 +2,9 @@
     <link rel="stylesheet" href="{{asset('css/services.css') }}">
 @endpush
 
-<div class="our-services">
-    <img src="/images/service-wave-up.png" width="100%" style="margin-top: -70px">
-    <div class="row mx-auto" style="max-width: 1800px">
-        <div class="col-12 mt-5">
+<div class="our-services" style="position: relative">
+    <div class="row mx-auto services-ident">
+        <div class="col-12">
             <p class="services-title text-center">Our services</p>
             <hr class="services-line">
         </div>
@@ -14,7 +13,8 @@
                 <p class="services-item-title">Sale of seedlings and paulownia trees</p>
             </div>
             <div class="mx-auto services-item-text-width">
-                <p class="services-item-text">Sale of seedlings and paulownia trees, Sale of seedlings and paulownia trees
+                <p class="services-item-text">Sale of seedlings and paulownia trees, Sale of seedlings and paulownia
+                    trees
                     Sale of seedlings and paulownia trees...</p>
                 <a class="services-item-link" href="#" target="_blank">Read more</a>
             </div>
@@ -34,8 +34,8 @@
                 <p class="services-item-title">ROI calculation</p>
             </div>
             <div class="mx-auto services-item-text-width pt-3">
-                <p class="services-item-text">Calculation of return on investment Calculation of return on investment
-                    Calculation of return on...</p>
+                <p class="services-item-text">Calculation of return on investment Calculation of return on
+                    investment Calculation of return on...</p>
                 <a class="services-item-link" href="#" target="_blank">Read more</a>
             </div>
         </div>
@@ -44,13 +44,14 @@
                 <p class="services-item-title">Consultation during the cultivation</p>
             </div>
             <div class="mx-auto services-item-text-width pt-2">
-                <p class="services-item-text">Consultation during the cultivation Consultation during the cultivation
-                    Consultation during the...</p>
+                <p class="services-item-text">Consultation during the cultivation Consultation during the
+                    cultivation Consultation during the...</p>
                 <a class="services-item-link" href="#" target="_blank">Read more</a>
             </div>
         </div>
     </div>
-
-
+    <div class="services-img-leaf-position">
+        <img src="{{asset('/images/service-leaf-tree.png')}}" class="img-leaf">
+    </div>
 </div>
-<img src="/images/service-wave-down.png" width="100%">
+

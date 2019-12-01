aec33e10163d99ef7eb0d315d806c30e4062afcb
s.stepaiko <stepaykosj@gmail.com>
2019-12-01 19:50:21 +0200

    Main index blade fix

--- resources/views/public/main/index.blade.php
+++ resources/views/public/main/index.blade.php
@@ -4,8 +4,7 @@
     @include('public.main.slider-sale')
     @include('public.main.about-us')
     @include('public.main.advantages')
-    @include('public.main.grades')
     @include('public.main.services')
-    @include('public.main.calculate')
+    @include('public.main.grades')
     @include('public.main.our-paulownia')
 @endsection

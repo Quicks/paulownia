61d4f3d228d6c83c7c7fb6d395999217e8018995
Orenchak <vitaorenchak@gmail.com>
2019-12-02 20:58:12 +0200

    changes

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -20,7 +20,7 @@
     {!! SEOMeta::generate() !!}
     @if(empty(SEOMeta::getTitle()))
         <title>{{ config('app.name', 'Laravel') }}</title>
-@endif
+@endifgi
 
 <!-- Styles -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">

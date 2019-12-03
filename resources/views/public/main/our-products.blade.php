2086245aaf899a7576a31872a48e50149fe4e531
Orenchak <vitaorenchak@gmail.com>
2019-12-03 21:15:25 +0200

    changes bugs

--- resources/views/public/main/our-products.blade.php
+++ resources/views/public/main/our-products.blade.php
@@ -2,13 +2,37 @@
     <link rel="stylesheet" href="{{asset('css/our-products.css') }}">
 @endpush
 
-<div class="main-content">
-    <div> <img src="/images/our-products-line-up.png" class="line-style"> </div>
+<div class="main-content" style="width:100%">
+
+    {{--<div> <img src="/images/our-products-line-up.png" class="line-style-up"> </div>--}}
+
         <div>
             <h1 class="about-title font-weight-bold text-center">Our products</h1>
             <hr class="line-for-about">
         </div>
 
+    {{--<div class="container">--}}
+    <div class="row main-class">
+        <div class="col-md-3 back-ground-img ">col</div>
 
-    <div> <img src="/images/our-products-line-down.png" class="line-style"> </div>
+        <div class="col back-ground-img">col</div>
+
+        <div class="col back-ground-img">col</div>
+
+        <div class="col back-ground-img">col</div>
+    </div>
+    <div class="w-100 h-25"></div>
+    <div class="row main-class">
+        <div class="col back-ground-img">col</div>
+
+        <div class="col back-ground-img">col</div>
+
+        <div class="col back-ground-img">col</div>
+
+        <div class="col back-ground-img">col</div>
+    </div>
+{{--</div>--}}
+
+    {{--<div> <img src="/images/our-products-line-down.png" class="line-style-down"> </div>--}}
+
 </div>

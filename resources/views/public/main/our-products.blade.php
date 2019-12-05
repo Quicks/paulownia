10eba8ff84de2ba30dbccf212588f99199188940
Orenchak <vitaorenchak@gmail.com>
2019-12-05 15:51:18 +0200

    changes bugs

--- resources/views/public/main/our-products.blade.php
+++ resources/views/public/main/our-products.blade.php
@@ -3,7 +3,6 @@
 @endpush
 
 <div class="main-content">
-
     {{--<div> <img src="/images/our-products-line-up.png" class="line-style-up"> </div>--}}
 
         <div >
@@ -11,242 +10,21 @@
             <hr class="line-for-about">
         </div>
 
-<div class="container-fluid">
-    <div class="row main-class">
-        <div class="back-ground-img position-relative " style="margin-left: 5%">
-            <img src="/images/our-products-col-fon.png" style="max-width:100%;position:relative;">
-                <div><img src="/images/line-for-goods-in-card.png" style="max-width:100%; margin-top:-8%;position: absolute;"></div>
 
-            <div>
-                <div  style="font-style: normal;
-                                font-weight: 800;
-                                font-size: 20px;
-                                line-height: 26px;
-                                padding-left: 3%;
-                                background-color: white;
-                                padding-top: 5%;
-                                box-sizing: border-box;
-                                /* or 130% */
-                                color: #8CBD02;"> Seedling Turbo Pro</div>
-
-                <div style="font-style: normal;
-                                font-weight: bold;
-                                font-size: 20px;
-                                line-height: 26px;
-                                 padding-left: 3%;
-                                  background-color: white;
-                                  box-sizing: border-box;
-                                /* or 130% */
-
-
-
-                                color: #8CBD02; "> 4.00 € </div>
+        <div class="row m-3">
+            <div class="col back-ground-img m-3">
 
             </div>
-
-        </div>
-
-        <div class="back-ground-img position-relative ">
-            <img src="/images/our-products-col-fon.png" style="max-width:100%;position:relative;">
-            <div><img src="/images/line-for-goods-in-card.png" style="max-width:100%; margin-top:-8%;position: absolute;"></div>
-
-            <div>
-                <div  style="font-style: normal;
-                                font-weight: 800;
-                                font-size: 20px;
-                                line-height: 26px;
-                                padding-left: 3%;
-                                background-color: white;
-                                padding-top: 5%;
-                                box-sizing: border-box;
-                                /* or 130% */
-                                color: #8CBD02;"> Seedling Turbo Pro</div>
-
-                <div style="font-style: normal;
-                                font-weight: bold;
-                                font-size: 20px;
-                                line-height: 26px;
-                                 padding-left: 3%;
-                                  background-color: white;
-                                  box-sizing: border-box;
-                                /* or 130% */
-
-
-
-                                color: #8CBD02; "> 4.00 € </div>
-
-            </div>
-
-        </div>
-
-        <div class="back-ground-img position-relative ">
-            <img src="/images/our-products-col-fon.png" style="max-width:100%;position:relative;">
-            <div><img src="/images/line-for-goods-in-card.png" style="max-width:100%; margin-top:-8%;position: absolute;"></div>
-
-            <div>
-                <div  style="font-style: normal;
-                                font-weight: 800;
-                                font-size: 20px;
-                                line-height: 26px;
-                                padding-left: 3%;
-                                background-color: white;
-                                padding-top: 5%;
-                                box-sizing: border-box;
-                                /* or 130% */
-                                color: #8CBD02;"> Seedling Turbo Pro</div>
-
-                <div style="font-style: normal;
-                                font-weight: bold;
-                                font-size: 20px;
-                                line-height: 26px;
-                                 padding-left: 3%;
-                                  background-color: white;
-                                  box-sizing: border-box;
-                                /* or 130% */
-
-
-
-                                color: #8CBD02; "> 4.00 € </div>
-
-            </div>
-
-        </div>
-
-        <div class="back-ground-img position-relative ">
-            <img src="/images/our-products-col-fon.png" style="max-width:100%;position:relative;">
-            <div><img src="/images/line-for-goods-in-card.png" style="max-width:100%; margin-top:-8%;position: absolute;"></div>
-
-            <div>
-                <div  style="font-style: normal;
-                                font-weight: 800;
-                                font-size: 20px;
-                                line-height: 26px;
-                                padding-left: 3%;
-                                background-color: white;
-                                padding-top: 5%;
-                                box-sizing: border-box;
-                                /* or 130% */
-                                color: #8CBD02;"> Seedling Turbo Pro</div>
-
-                <div style="font-style: normal;
-                                font-weight: bold;
-                                font-size: 20px;
-                                line-height: 26px;
-                                 padding-left: 3%;
-                                  background-color: white;
-                                  box-sizing: border-box;
-                                /* or 130% */
-
-
-
-                                color: #8CBD02; "> 4.00 € </div>
-
-            </div>
-
-        </div>
-
-        <div class="back-ground-img position-relative ">
-            <img src="/images/our-products-col-fon.png" style="max-width:100%;position:relative;">
-            <div><img src="/images/line-for-goods-in-card.png" style="max-width:100%; margin-top:-8%;position: absolute;"></div>
-
-            <div>
-                <div  style="font-style: normal;
-                                font-weight: 800;
-                                font-size: 20px;
-                                line-height: 26px;
-                                padding-left: 3%;
-                                background-color: white;
-                                padding-top: 5%;
-                                box-sizing: border-box;
-                                /* or 130% */
-                                color: #8CBD02;"> Seedling Turbo Pro</div>
-
-                <div style="font-style: normal;
-                                font-weight: bold;
-                                font-size: 20px;
-                                line-height: 26px;
-                                 padding-left: 3%;
-                                  background-color: white;
-                                  box-sizing: border-box;
-                                /* or 130% */
-
-
-
-                                color: #8CBD02; "> 4.00 € </div>
-
-            </div>
-
-        </div>
-
-        <div class="back-ground-img position-relative ">
-            <img src="/images/our-products-col-fon.png" style="max-width:100%;position:relative;">
-            <div><img src="/images/line-for-goods-in-card.png" style="max-width:100%; margin-top:-8%;position: absolute;"></div>
-
-            <div>
-                <div  style="font-style: normal;
-                                font-weight: 800;
-                                font-size: 20px;
-                                line-height: 26px;
-                                padding-left: 3%;
-                                background-color: white;
-                                padding-top: 5%;
-                                box-sizing: border-box;
-                                /* or 130% */
-                                color: #8CBD02;"> Seedling Turbo Pro</div>
-
-                <div style="font-style: normal;
-                                font-weight: bold;
-                                font-size: 20px;
-                                line-height: 26px;
-                                 padding-left: 3%;
-                                  background-color: white;
-                                  box-sizing: border-box;
-                                /* or 130% */
-
-
-
-                                color: #8CBD02; "> 4.00 € </div>
-
-            </div>
-
-        </div>
-        <div class="back-ground-img position-relative ">
-            <img src="/images/our-products-col-fon.png" style="max-width:100%;position:relative;">
-            <div><img src="/images/line-for-goods-in-card.png" style="max-width:100%; margin-top:-8%;position: absolute;"></div>
-
-            <div>
-                <div  style="font-style: normal;
-                                font-weight: 800;
-                                font-size: 20px;
-                                line-height: 26px;
-                                padding-left: 3%;
-                                background-color: white;
-                                padding-top: 5%;
-                                box-sizing: border-box;
-                                /* or 130% */
-                                color: #8CBD02;"> Seedling Turbo Pro</div>
-
-                <div style="font-style: normal;
-                                font-weight: bold;
-                                font-size: 20px;
-                                line-height: 26px;
-                                 padding-left: 3%;
-                                  background-color: white;
-                                  box-sizing: border-box;
-                                /* or 130% */
-
-
-
-                                color: #8CBD02; "> 4.00 € </div>
-
-            </div>
-
+            <div class="col border bg-light m-3">Пользовательская вставка столбцов</div>
+            <div class="col border bg-light m-3">Пользовательская вставка столбцов</div>
+            <div class="col border bg-light m-3">Пользовательская вставка столбцов</div>
         </div>
 
 
-    </div>
-</div>
 
+
+
+    {{--<div> <img src="/images/our-products-line-down.png" class="line-style-down"> </div>--}}
 </div>
 
     {{--<div class="row main-class align-items-center">--}}
@@ -275,6 +53,6 @@
 
     {{--</div>--}}
 
-    {{--<div> <img src="/images/our-products-line-down.png" class="line-style-down"> </div>--}}
+
 
 {{--</div>--}}

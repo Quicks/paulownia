b933c691e4dfcba33c53e91f1a29f94e8418b808
Orenchak <vitaorenchak@gmail.com>
2019-12-07 14:47:30 +0200

    changes bugs

--- resources/views/public/products/product-card.blade.php
+++ resources/views/public/products/product-card.blade.php
@@ -1,5 +1,5 @@
 @push('css')
-    <link rel="stylesheet" href="{{asset('css/our-products.css') }}">
+    <link rel="stylesheet" href="{{asset('css/product-card.css') }}">
 @endpush
 
     <div class="col back-ground-img m-3 position-relative">
@@ -8,7 +8,7 @@
         <img src="{{asset('/images/our-products-like.png')}}" class="position-absolute like-product">
         <img src="{{asset('/images/our-products-col-fon.png')}}" style="border-radius: 25px 25px 0 0;max-width: 100%">
         <img src="{{asset('/images/line-for-goods-in-card.png')}}" style="margin-top:-8%;max-width: 100%">
-        <div class="title-for-card">Seedling Turbo Pro</div>
+        <a href="#" class="title-for-card">Seedling Turbo Pro</a>
         <div class="price-for-card">4.00 €</div>
         <div class="text-card">Description, description,
             description, description, description,

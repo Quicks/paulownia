bf0133fa236272f3c7120e6d8c72ff98b21a8b22
Orenchak <vitaorenchak@gmail.com>
2019-12-07 15:52:43 +0200

    changes bugs

--- resources/views/public/products/product-card.blade.php
+++ resources/views/public/products/product-card.blade.php
@@ -6,8 +6,8 @@
         <div class="position-absolute product-sale">-3%</div>
         <img src="{{asset('/images/our-products-box.png')}}" class="position-absolute box-product">
         <img src="{{asset('/images/our-products-like.png')}}" class="position-absolute like-product">
-        <img src="{{asset('/images/our-products-col-fon.png')}}" style="border-radius: 25px 25px 0 0;max-width: 100%">
-        <img src="{{asset('/images/line-for-goods-in-card.png')}}" style="margin-top:-8%;max-width: 100%">
+        <img src="{{asset('/images/our-products-col-fon.png')}}" style="border-radius: 25px 25px 0 0;width:100%">
+        <img src="{{asset('/images/line-for-goods-in-card.png')}}" style="margin-top:-8%;width:100%">
         <a href="#" class="title-for-card">Seedling Turbo Pro</a>
         <div class="price-for-card">4.00 €</div>
         <div class="text-card">Description, description,

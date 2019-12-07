50c2b3964e79011198ac2bcee0f3ff99f0e667dc
Orenchak <vitaorenchak@gmail.com>
2019-12-07 14:51:40 +0200

    changes bugs

--- resources/views/public/main/our-products.blade.php
+++ resources/views/public/main/our-products.blade.php
@@ -2,6 +2,7 @@
     <link rel="stylesheet" href="{{asset('css/our-products.css') }}">
 @endpush
 
+
 <div class="main-content">
 
     <div><img src="/images/our-products-line-up.png" class="line-style-up"></div>

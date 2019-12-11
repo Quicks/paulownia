2dd19cf5d6100b998fea5ba5818e692b81715f8b
Smirnova <smirnova270694@gmail.com>
2019-12-11 02:01:42 +0200

    added products to goods

--- resources/views/public/products/index.blade.php
+++ resources/views/public/products/index.blade.php
@@ -1,4 +1,10 @@
 @extends('layouts.public')
 @section('content')
-    <h1>Goods page</h1>
+    <div class="row m-3 justify-content-center">
+        @foreach($products as $product)
+            @include('public.products.product-card', ['product' => $product])
+            <a href="{{ url('/products/' . $product->product_id) }}">view product</a>
+        @endforeach
+    </div>
+
 @endsection

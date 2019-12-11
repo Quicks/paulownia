6a02f4b8469c61d1eb9fb8d914f0d6bbb16b3a1a
Orenchak <vitaorenchak@gmail.com>
2019-12-11 20:23:51 +0200

    changes bugs

--- resources/views/public/main/slider-sale-about-us.blade.php
+++ resources/views/public/main/slider-sale-about-us.blade.php
@@ -125,56 +125,34 @@
         <hr class="line-about-under">
     </div>
 
-<<<<<<< HEAD
-    <div class="row mx-auto back-ground-main pt-xl-5 pb-xl-5 p-3" style="max-width:1200px;">
-
-        <div class="col-xl-6 col-md-6 col-sm-12 back-ground-1 mx-auto" style="max-width:569px;">
-
-            <img class="lazyload" data-src="/images/logo.png" style="max-width:35%;margin-top: 3%">
-=======
             <div class="row mx-auto back-ground-main pt-xl-5 pb-xl-5 p-3" style="max-width:1300px;">
-
                 <div class="col-xl-6 col-md-12 col-sm-12 back-ground-1 mx-auto mt-5 mb-5">
-
                     <img class="lazyload img-logo-1" data-src="/images/logo.png">
->>>>>>> 5d87a74de52539f077073aa97a481682c278e9ce
-
-            <p class="about-text">Paulownia Professional is a manufacturer of seedlings, popular species of Paulownia.
-                Our company has settled in Spain, the province of Aragon, whose production area is 2 hectares,
-                which allows us to produce more than half a million seedlings per year.
-                We offer an individual approach to each client,
-                a large selection of advanced varieties of Paulownia. We will also help you determine the variety that best suits your climate zone.
-                We set ourselves the task of growing high-quality,
-                certified seedlings that can satisfy the full range of applications of Paulownia...
-                <a href="#" class="about-read-m" >Read more</a>
-            </p>
-
-<<<<<<< HEAD
-        </div>
-        <div class="col-xl-6 col-md-6 col-sm-12 back-ground-2 mx-auto" style="max-width:569px;">
-=======
+                    <p class="about-text">Paulownia Professional is a manufacturer of seedlings, popular species of Paulownia.
+                        Our company has settled in Spain, the province of Aragon, whose production area is 2 hectares,
+                        which allows us to produce more than half a million seedlings per year.
+                        We offer an individual approach to each client,
+                        a large selection of advanced varieties of Paulownia. We will also help you determine the variety that best suits your climate zone.
+                        We set ourselves the task of growing high-quality,
+                        certified seedlings that can satisfy the full range of applications of Paulownia...
+                        <a href="#" class="about-read-m" >Read more</a>
+                    </p>
                 </div>
 
+                 <div class="col-xl-6 col-md-6 col-sm-12 back-ground-2 mx-auto" style="max-width:569px;">
                     <div class="col-xl-6 col-md-12 col-sm-12 back-ground-2 mx-auto mt-5 mb-5" >
-
                         <img class="lazyload img-logo-2 " data-src="/images/logo.png">
->>>>>>> 5d87a74de52539f077073aa97a481682c278e9ce
-
-            <img class="lazyload" data-src="/images/logo.png"  style="max-width:35%;float: right;margin:3% 2% 8% 50%;">
-
-            <p class="about-text">Paulownia Professional is a manufacturer of seedlings, popular species of Paulownia.
-                Paulownia Professiona's goal is to contribute to the improvement of environmental conditions in Aragon,
-                Spain and Europe as a whole !!! The creation of plantations and industrialization of Pavlovia,
-                as well as the creation of "green zones" and parks, will allow to save eco-resources, solve energy consumption
-                issues without environmental risks, increase the volume of animal feed production, globally
-                deal with emissions into the atmosphere, industrial enterprises, large amounts of CO2, toxic substances, heavy metals..
-                <a  href="#" class="about-read-m" >Read more</a>
-            </p>
-
-
-        </div>
-
-    </div>
+                        <p class="about-text">Paulownia Professional is a manufacturer of seedlings, popular species of Paulownia.
+                            Paulownia Professiona's goal is to contribute to the improvement of environmental conditions in Aragon,
+                            Spain and Europe as a whole !!! The creation of plantations and industrialization of Pavlovia,
+                            as well as the creation of "green zones" and parks, will allow to save eco-resources, solve energy consumption
+                            issues without environmental risks, increase the volume of animal feed production, globally
+                            deal with emissions into the atmosphere, industrial enterprises, large amounts of CO2, toxic substances, heavy metals..
+                            <a  href="#" class="about-read-m" >Read more</a>
+                        </p>
+                    </div>
+                 </div>
+            </div>
 
 </div>
 

063e770733b648af7280912593abf650d4957f15
Orenchak <vitaorenchak@gmail.com>
2019-12-12 20:38:53 +0200

    delete bugs

---
+++ resources/views/public/products/breadcrumbs.blade.php
@@ -0,0 +1,9 @@
+@push('css')
+    <link rel="stylesheet" href="{{asset('css/breadcrumbs.css') }}?v1">
+@endpush
+
+<div class="row">
+    <div class="col-12 fon-text pl-5 pt-4">
+        <a href="#" class="fon-text">Main</a>/<a href="#" class="fon-text" >Goods</a>
+    </div>
+</div>
--- resources/views/public/products/index.blade.php
+++ resources/views/public/products/index.blade.php
@@ -1,5 +1,8 @@
 @extends('layouts.public')
 @section('content')
+
+    @include('public.products.breadcrumbs')
+
     <div class="row m-3 justify-content-center">
         @foreach($products as $product)
             @include('public.products.product-card', ['product' => $product])

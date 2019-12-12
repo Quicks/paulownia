263b94bec69b748924dd86a189bda47b06b112f3
Smirnova <smirnova270694@gmail.com>
2019-12-12 15:56:21 +0200

    fixed products view

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
 use Webkul\Core\Repositories\SliderRepository;
 use App\Models\Gallery;
 use Webkul\Product\Models\ProductFlat;
@@ -13,9 +14,8 @@ class MainController extends Controller
     {
         $currentChannel = core()->getCurrentChannel();
         $sliderData = $sliderRepository->findByField('channel_id', $currentChannel->id)->toArray();
-
         $mainGallery = Gallery::first();
-        $products = ProductFlat::where('featured', 1)->where('status', 1)->limit(8)->get();
+        $products = ProductFlat::where('featured', 1)->where('status', 1)->where('locale', App::getLocale())->limit(8)->get()->sortByDesc('special_price');
 
         return view('public.main.index', compact('sliderData', 'mainGallery', 'products'));
     }

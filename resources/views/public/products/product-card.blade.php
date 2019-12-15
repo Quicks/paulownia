2084f0a53968a6a591a69351d64bb917d892e0d2
Orenchak <vitaorenchak@gmail.com>
2019-12-15 20:31:28 +0200

    delete bugs

--- resources/views/public/products/product-card.blade.php
+++ resources/views/public/products/product-card.blade.php
@@ -18,24 +18,22 @@
 
     <img data-src="{{asset('/images/line-for-goods-in-card.png')}}" class="lazyload line-product-card">
         <div class="row m-0">
-            <span class="col-6 title-for-card">hgshghkghk</span>
-            <a href="#" class="col-2 ml-5 mt-2">
+
+            <a href="{{route('public.products.show', $product->product_id)}}" class="col-6 title-for-card">{{$product->name}}</a>
+            <a href="#" class="col-2 mt-1 ml-5 box-m">
                 <img data-src="{{asset('/images/our-products-box.png')}}" class="box-product lazyload">
             </a>
-            <a href="#" class="col-2 pl-1 mt-2">
-                 <img data-src="{{asset('/images/our-products-like.png')}}" class="lazyload like-product" style="border-radius:50%;">
+            <a href="#" class="col-2 mt-1 pl-0 like-m">
+                <img data-src="{{asset('/images/our-products-like.png')}}" class="like-product lazyload" style="border-radius:50%">
             </a>
-            <div class="price-for-card">3€</div>
+
+            @if($product->special_price != 0)
+            <div class="price-for-card col-12">{{number_format($product->special_price, 2)}} €</div>
+            @else
+            <div class="price-for-card col-12">{{number_format($product->price, 2)}} €</div>
+            @endif
 
         </div>
-
-
-    {{--<a href="{{route('public.products.show', $product->product_id)}}" class="title-for-card">{{$product->name}}</a>--}}
-    {{--@if($product->special_price != 0)--}}
-        {{--<div class="price-for-card">{{number_format($product->special_price, 2)}} €</div>--}}
-    {{--@else--}}
-        {{--<div class="price-for-card">{{number_format($product->price, 2)}} €</div>--}}
-    {{--@endif--}}
     <div class="text-card">{{substr(strip_tags($product->short_description), 0, 90)}}</div>
 </div>
 

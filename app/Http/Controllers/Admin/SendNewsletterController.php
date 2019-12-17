690967fbf2cd5d4120c9cd4097dc64c8677af378
s.stepaiko <stepaykosj@gmail.com>
2019-12-17 17:40:44 +0200

    Send newsletter prototype added

--- app/Http/Controllers/Admin/SendNewsletterController.php
+++ app/Http/Controllers/Admin/SendNewsletterController.php
@@ -4,6 +4,8 @@ namespace App\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\NewsLetterEmail;
 
 class SendNewsletterController extends Controller
 {
@@ -14,8 +16,26 @@ class SendNewsletterController extends Controller
 
     public function send(Request $request)
     {
-       
-        return dd($request);//view('admin.sendNewsletter.index', ["message" => "Hi SSJ"]);
+        $this->validate($request, [
+            'subject' => 'required|max:200',
+            'text' => 'required|string',
+        ]);
+
+        $newsData['subject'] = $request->subject;
+        $newsData['text'] = $request->text;
+
+        $mailSent = true;
+        try {
+            Mail::queue(new NewsLetterEmail($newsData));
+
+            session()->flash('success', 'E-mail sended');
+        } catch (\Exception $e) {
+            session()->flash('error', 'E-mail send error');
+            $mailSent = false;
+            dd('ERR', $e);
+        }
+
+        return $mailSent ? 'Sent' : 'Error';//view('admin.sendNewsletter.index', ["message" => "Hi SSJ"]);
     }
 
 }

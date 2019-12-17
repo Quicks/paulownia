92684bc22ec317e542bed661a1d16d273981c887
Orenchak <vitaorenchak@gmail.com>
2019-12-17 20:54:05 +0200

    delete bugs

--- resources/views/public/products/index.blade.php
+++ resources/views/public/products/index.blade.php
@@ -3,11 +3,4 @@
     @include('public.products.breadcrumbs')
     @include('public.products.ticker')
     @include('public.products.download-price')
-
-    <div class="row m-3 justify-content-center">
-        @foreach($products as $product)
-            @include('public.products.product-card', ['product' => $product])
-        @endforeach
-    </div>
-
 @endsection

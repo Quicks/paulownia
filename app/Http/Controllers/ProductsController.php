cd81c77f4d89fb797e71f1b9afec0611e2d1298e
Smirnova <smirnova270694@gmail.com>
2019-12-18 21:15:05 +0200

    added filter by category

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -6,14 +6,22 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
 use Webkul\Product\Models\ProductFlat;
 use App\Models\Content;
+use Webkul\Category\Models\Category;
+use Webkul\Category\Models\CategoryTranslation;
+use Webkul\Product\Repositories\ProductRepository;
 
 class ProductsController extends Controller
 {
-    public function index()
+    public function index(Request $request, ProductRepository $product)
     {
         $products = ProductFlat::where('status', 1)->where('locale', App::getLocale())->get()->sortByDesc('special_price');
         $ticker = Content::where('name', 'products_ticker')->first();
-        return view('public.products.index', compact('products', 'ticker'));
+        $categories = Category::all();
+            if($request->has('category')){
+                $category_id = CategoryTranslation::where('slug', $request->category)->first()->category_id;
+                $products = $product->getAll($category_id)->sortByDesc('special_price');
+            }
+        return view('public.products.index', compact('products', 'ticker', 'categories'));
     }
 
     public function show($url_key)

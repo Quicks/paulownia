972eaa59799656a644cf950477422aa4f461e38a
Orenchak <vitaorenchak@gmail.com>
2019-12-18 16:23:55 +0200

    delete bugs

--- resources/views/public/breadcrumbs.blade.php
+++ resources/views/public/breadcrumbs.blade.php
@@ -1,5 +1,5 @@
 @push('css')
-    <link rel="stylesheet" href="{{asset('css/breadcrumbs.css') }}?v1">
+    <link rel="stylesheet" href="{{asset('css/breadcrumbs.css') }}?v2">
 @endpush
 
 <div class="row fon-text-title" >

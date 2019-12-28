2fd3d4ba8cdb09d7c87d71e904e1f0d704d0caae
Smirnova <smirnova270694@gmail.com>
2019-12-28 22:33:42 +0200

    fixed products controller

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -14,22 +14,20 @@ class ProductsController extends Controller
 {
     public function index(Request $request, ProductRepository $product)
     {
-        if(!$request->has('category') && !$request->has('type')){
-            $products = $product->getAll()->sortByDesc('special_price');
-        }
         $ticker = Content::where('name', 'products_ticker')->first();
         $categories = Category::where('status', 1)->get();
         $types = Attribute::where('code', 'type_of_paulownia')->first()->options()->get();
-        if($request->has('category')){
+        $category_id = null;
+        if (!$request->has('category') && !$request->has('type')) {
+            $products = $product->getAll()->sortByDesc('special_price');
+        }
+        if ($request->has('category')) {
             $category_id = Category::whereTranslation('slug', $request->category)->first()->id;
+        }
+        if (!$request->has('type')) {
             $products = $product->getAll($category_id)->sortByDesc('special_price');
         }
-        if($request->has('type')){
-            if ($request->has('category')){
-                $category_id = Category::whereTranslation('slug', $request->category)->first()->id;
-            } else {
-                $category_id = null;
-            }
+        if ($request->has('type')) {
             $products = $product->getAll($category_id)->where('type_of_paulownia_label', $request->type)->sortByDesc('special_price');
         }
         return view('public.products.index', compact('products', 'ticker', 'categories', 'types'));

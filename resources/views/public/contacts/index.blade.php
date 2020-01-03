d9dad1468bfe3e69c8b5a412be0f1795324bad57
Orenchak <vitaorenchak@gmail.com>
2020-01-03 21:28:15 +0200

    adaptive layout

--- resources/views/public/contacts/index.blade.php
+++ resources/views/public/contacts/index.blade.php
@@ -5,13 +5,16 @@
         <link rel="stylesheet" href="{{asset('css/contacts.css') }}">
     @endpush
 
-    <div class="row m-0 fon-for-contacts">
+    <div class="row m-0 fon-for-contacts position-relative">
 
         <div class="col-12 line-for-contacts ">@include('public.breadcrumbs', $breadcrumbs = [route('public.contacts.index') => 'header-footer.contacts' ])</div>
 
-        <div class="row m-0">
+       <div class="position-absolute leaf-for-contacts"><img class="lazyload" data-src="{{asset('/images/leaf-for-contacts.png')}}" style="max-width: 100%"></div>
 
-            <div class="col-xl-5 col-md-12 col-sm-12 pl-5 pr-0 pt-5 ">
+        <div class="row m-0 position-relative">
+
+            <div class="col-xl-5 col-md-12 col-sm-12 pl-xl-5 pl-md-3 pl-sm-2 pr-xl-0 pr-md-1 pr-sm-1 pt-5 ">
+
                 <div class="title-for-contacts mb-5">@lang('contacts.spain')</div>
                 <div class="text-for-contacts mb-5 ">@lang('contacts.text').</div>
                 <div class="title-for-contacts mb-5">PAULOWNIA PROFFESIONAL S.L.</div>
@@ -34,24 +37,23 @@
             </div>
 
 
-            <div class="col-xl-7 col-md-12 col-sm-12 pl-5 pt-5 pb-5">
-                <img data-src="{{asset('/images/map-with-Archi.png')}}" class="lazyload map-with-Archi">
+            <div class="col-xl-7 col-md-12 col-sm-12 pl-xl-5 pl-md-5 pl-sm-4 pt-5 pb-5 position-relative ">
+                <img data-src="{{asset('/images/map-with-Archi.png')}}" class="lazyload map-with-Archi position-relative">
             </div>
 
         </div>
-                    <div class="col-xl-12 col-md-12 col-sm-4 pl-5 m-0">
 
-                        <span class="title-for-contacts ">@lang('contacts.write-to-us'):</span>
-                        <input placeholder="Your name" class="email-contacts">
-                        <input placeholder="Email" class="email-contacts ">
-                        <input placeholder="@lang('contacts.your-message')" class="email-contacts ">
 
+                    <div class="col-xl-12 col-md-12 col-sm-4 pl-5 mx-auto pb-5">
+
+                        <span class="title-for-contacts name-contacts">@lang('contacts.write-to-us'):</span>
+                        <input placeholder="Your name" class="email-contacts ">
+                        <input placeholder="Email"     class="email-contacts">
+                        <input placeholder="@lang('contacts.your-message')" class="email-contacts messege-contacts">
                         <button type="submit" class="button-contacts ">@lang('contacts.send')</button>
 
-                     </div>
+                    </div>
     </div>
-
-
 
 
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -92,6 +92,7 @@ Route::group(['middleware' => ['web']], function () {
         Route::get('/about-us', 'App\Http\Controllers\AboutUsController@index')->name('public.about-us.index');
         Route::get('/paulownia', 'App\Http\Controllers\PaulowniaController@index')->name('public.paulownia.index');
         Route::get('/term-of-sale', 'App\Http\Controllers\TermsOfSaleController@index')->name('public.terms-of-sale.index');
+
     });
 
     Route::get('/certificate/{code}', 'App\Http\Controllers\CertificateController')->name('certificate');

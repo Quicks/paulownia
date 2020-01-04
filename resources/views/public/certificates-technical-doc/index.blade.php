59fd4b2b6ba47295823e78efc092fe8afa2bc670
Orenchak <vitaorenchak@gmail.com>
2020-01-04 23:27:14 +0200

    create route

--- resources/views/public/certificates-technical-doc/index.blade.php
+++ resources/views/public/certificates-technical-doc/index.blade.php
@@ -2,8 +2,22 @@
 @section('content')
 
     @push('css')
-        <link rel="stylesheet" href="{{asset('css/contacts.css') }}">
+        <link rel="stylesheet" href="{{asset('css/partners.css') }}">
     @endpush
 
 
+    <div class="col-12 mb-5">@include('public.breadcrumbs', $breadcrumbs = [route('public.products.index') =>'header-footer.about us')]</div>
+
+    <div class="row fon-for-partners m-0">
+        <div class="col-12 partners-background position-relative">
+            <div>
+                <img class="img-leaf lazyload position-absolute" data-src="{{asset('/images/service-leaf-tree.png')}}">
+            </div>
+            <div class="partner-text">@lang('about-us-header.our-partners')</div>
+        </div>
+
+        <div class="row mx-auto justify-content-center">
+
+        </div>
+    </div>
 @endsection

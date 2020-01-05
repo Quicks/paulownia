e73e2dc32eb1c55615db4743d1a9c4f423079d02
Orenchak <vitaorenchak@gmail.com>
2020-01-05 20:09:45 +0200

    create table

--- resources/views/public/cart/index.blade.php
+++ resources/views/public/cart/index.blade.php
@@ -8,15 +8,23 @@
     <div class="row fon-cart mx-auto">
         <div class="col-12 ">@include('public.breadcrumbs', $breadcrumbs = [route('public.products.index') => 'header-footer.cart' ])</div>
 
+        <div class="col-6"></div>
+        <div class="col-2 text-center text-title-cart mt-xl-3">Price for 1 item:</div>
+        <div class="col-2 text-center text-title-cart mt-xl-3">Amount:</div>
+        <div class="col-2 text-center text-title-cart mt-xl-3">Total:</div>
+
+
         <div class="col-3 text-center">
             <div><img data-src="{{asset('/images/img-cart.png')}}" class="lazyload img-cart "></div>
         </div>
         <div class="col-3">
-            <div class="text-cart">Seedling paulownia Elongata</div>
+            <div class="text-name-cart pt-xl-5">Seedling paulownia Elongata</div>
         </div>
-        <div class="col-2 text-center">3,15 €</div>
-        <div class="col-2 text-center"> 1 </div>
-        <div class="col-2 text-center">3,15 €</div>
+        <div class="col-2 text-center pt-xl-5">3,15 €</div>
+        <div class="col-2 text-center pt-xl-5"> 1 </div>
+        <div class="col-2 text-center pt-xl-5">3,15 €</div>
+
+
 
 
     </div>

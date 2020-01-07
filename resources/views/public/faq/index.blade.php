7cac2008d5464aefa70c18ff944b96d06a9fff31
Orenchak <vitaorenchak@gmail.com>
2020-01-07 13:45:21 +0200

    create layout

--- resources/views/public/faq/index.blade.php
+++ resources/views/public/faq/index.blade.php
@@ -1,4 +1,12 @@
 @extends('layouts.public')
 @section('content')
-    <h1>FAQ page</h1>
+
+    <div class="row m-0 p-0 ">
+
+        <div class="col-12">@include('public.breadcrumbs', $breadcrumbs = [route('public.faq.index') => 'header-footer.about us' ])</div>
+
+    </div>
+
+
+
 @endsection

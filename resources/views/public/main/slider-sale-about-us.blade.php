3599546dcad426524c9c27f6dc5e4ebf393e772f
Orenchak <vitaorenchak@gmail.com>
2020-01-07 14:48:04 +0200

    create layout

--- resources/views/public/main/slider-sale-about-us.blade.php
+++ resources/views/public/main/slider-sale-about-us.blade.php
@@ -127,7 +127,7 @@
 
             <div class="row mx-auto back-ground-main pt-xl-5 pb-xl-5 p-3 back-max-width ">
 
-                    <div class="col-xl-6 col-md-12 col-sm-12 back-ground-1 mx-auto mt-5 mb-5">
+                    <div class="col-xl-6 col-md-12 col-sm-12 back-ground-1 mx-auto mt-5 mb-5 ">
                         <img class="lazyload img-logo-1" data-src="/images/logo.png">
                         <p class="about-text">Paulownia Professional is a manufacturer of seedlings, popular species of Paulownia.
                             Our company has settled in Spain, the province of Aragon, whose production area is 2 hectares,

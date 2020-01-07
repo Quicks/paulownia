ddb2a0440affb43aaadbd7577a0a2edfdae21704
s.stepaiko <stepaykosj@gmail.com>
2020-01-07 14:24:04 +0200

    Product pagination limit is set to 1000 products a page

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -14,6 +14,7 @@ class ProductsController extends Controller
 {
     public function index(Request $request, ProductRepository $product)
     {
+        $request->request->add(['limit' => '1000']); //product pagination limit is set to 1000 products a page
         $ticker = Content::where('name', 'products_ticker')->first();
         $categories = Category::where('status', 1)->get();
         $types = Attribute::where('code', 'type_of_paulownia')->first()->options()->get();

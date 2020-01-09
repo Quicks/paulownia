e9d016998041b583802a908370fc039f9b73fa77
Orenchak <vitaorenchak@gmail.com>
2020-01-09 16:50:15 +0200

    fix bag.

--- resources/views/public/main/services.blade.php
+++ resources/views/public/main/services.blade.php
@@ -58,54 +58,54 @@
                         </div>
                     </div>
                 </div>
-                {{--<div class="carousel-item ">--}}
-                    {{--<div class="row">--}}
-                        {{--<div class="col-xl-3 col-md-12 col-sm-12 services-1 mx-auto services-item-width mb-3">--}}
-                            {{--<div class="mx-auto" style="max-width: 190px">--}}
-                                {{--<p class="services-item-title">Sale of seedlings and paulownia trees</p>--}}
-                            {{--</div>--}}
-                            {{--<div class="mx-auto services-item-text-width">--}}
-                                {{--<p class="services-item-text-1-2">Sale of seedlings and paulownia trees, Sale of seedlings--}}
-                                    {{--and paulownia--}}
-                                    {{--trees--}}
-                                    {{--Sale of seedlings and paulownia trees...</p>--}}
-                                {{--<a class="services-item-link" href="{{route('public.service.index')}} ">Read more</a>--}}
-                            {{--</div>--}}
-                        {{--</div>--}}
-                        {{--<div class="col-xl-3 col-md-12 col-sm-12 services-2 mx-auto services-item-width mb-3">--}}
-                            {{--<div class="mx-auto" style="max-width: 190px">--}}
-                                {{--<p class="services-item-title">Analysis and personal design future plantations</p>--}}
-                            {{--</div>--}}
-                            {{--<div class="mx-auto services-item-text-width">--}}
-                                {{--<p class="services-item-text-1-2">Analysis and personal design of future plantations--}}
-                                    {{--Analysis and personal design of future plantations...</p>--}}
-                                {{--<a class="services-item-link" href="#">Read more</a>--}}
-                            {{--</div>--}}
-                        {{--</div>--}}
-                        {{--<div class="col-xl-3 col-md-12 col-sm-12 services-3 mx-auto services-item-width mb-3">--}}
-                            {{--<div class="mx-auto pt-3" style="max-width: 190px">--}}
-                                {{--<p class="services-item-title">ROI calculation</p>--}}
-                            {{--</div>--}}
-                            {{--<div class="mx-auto services-item-text-width pt-3">--}}
-                                {{--<p class="services-item-text-3-4">Calculation of return on investment Calculation of return--}}
-                                    {{--on--}}
-                                    {{--investment Calculation of return on...</p>--}}
-                                {{--<a class="services-item-link" href="{{route('public.calculations.index')}}">Read more</a>--}}
-                            {{--</div>--}}
-                        {{--</div>--}}
-                        {{--<div class="col-xl-3 col-md-12 col-sm-12 services-4 mx-auto services-item-width mb-3">--}}
-                            {{--<div class="mx-auto" style="max-width: 190px">--}}
-                                {{--<p class="services-item-title">Consultation during the cultivation</p>--}}
-                            {{--</div>--}}
-                            {{--<div class="mx-auto services-item-text-width pt-2">--}}
-                                {{--<p class="services-item-text-3-4">Consultation during the cultivation Consultation during--}}
-                                    {{--the--}}
-                                    {{--cultivation Consultation during the...</p>--}}
-                                {{--<a class="services-item-link" href="#">Read more</a>--}}
-                            {{--</div>--}}
-                        {{--</div>--}}
-                    {{--</div>--}}
-                {{--</div>--}}
+                <div class="carousel-item ">
+                    <div class="row">
+                        <div class="col-xl-3 col-md-12 col-sm-12 services-1 mx-auto services-item-width mb-3">
+                            <div class="mx-auto" style="max-width: 190px">
+                                <p class="services-item-title">Sale of seedlings and paulownia trees</p>
+                            </div>
+                            <div class="mx-auto services-item-text-width">
+                                <p class="services-item-text-1-2">Sale of seedlings and paulownia trees, Sale of seedlings
+                                    and paulownia
+                                    trees
+                                    Sale of seedlings and paulownia trees...</p>
+                                <a class="services-item-link" href="{{route('public.service.index')}} ">Read more</a>
+                            </div>
+                        </div>
+                        <div class="col-xl-3 col-md-12 col-sm-12 services-2 mx-auto services-item-width mb-3">
+                            <div class="mx-auto" style="max-width: 190px">
+                                <p class="services-item-title">Analysis and personal design future plantations</p>
+                            </div>
+                            <div class="mx-auto services-item-text-width">
+                                <p class="services-item-text-1-2">Analysis and personal design of future plantations
+                                    Analysis and personal design of future plantations...</p>
+                                <a class="services-item-link" href="#">Read more</a>
+                            </div>
+                        </div>
+                        <div class="col-xl-3 col-md-12 col-sm-12 services-3 mx-auto services-item-width mb-3">
+                            <div class="mx-auto pt-3" style="max-width: 190px">
+                                <p class="services-item-title">ROI calculation</p>
+                            </div>
+                            <div class="mx-auto services-item-text-width pt-3">
+                                <p class="services-item-text-3-4">Calculation of return on investment Calculation of return
+                                    on
+                                    investment Calculation of return on...</p>
+                                <a class="services-item-link" href="{{route('public.calculations.index')}}">Read more</a>
+                            </div>
+                        </div>
+                        <div class="col-xl-3 col-md-12 col-sm-12 services-4 mx-auto services-item-width mb-3">
+                            <div class="mx-auto" style="max-width: 190px">
+                                <p class="services-item-title">Consultation during the cultivation</p>
+                            </div>
+                            <div class="mx-auto services-item-text-width pt-2">
+                                <p class="services-item-text-3-4">Consultation during the cultivation Consultation during
+                                    the
+                                    cultivation Consultation during the...</p>
+                                <a class="services-item-link" href="#">Read more</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
             <a class="carousel-control-prev services-prev" href="#carouselExampleIndicators1" role="button" data-slide="prev">
                 <img class="lazyload" data-src="{{asset('/images/arrow-left.svg')}}">

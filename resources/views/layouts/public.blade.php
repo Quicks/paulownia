04ba1b6867095acee784b2f14c53b10265fc54f9
s.stepaiko <stepaykosj@gmail.com>
2020-01-16 11:06:51 +0200

    Favicons added

--- resources/views/layouts/public.blade.php
+++ resources/views/layouts/public.blade.php
@@ -20,7 +20,14 @@
     {!! SEOMeta::generate() !!}
     @if(empty(SEOMeta::getTitle()))
         <title>{{ config('app.name', 'Laravel') }}</title>
-@endif
+    @endif
+
+<!-- Favicons -->
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('images/favicons/apple-touch-icon.png') }}">
+    <link rel="icon" type="image/png" sizes="32x32" href="{{ asset('images/favicons/favicon-32x32.png') }}">
+    <link rel="icon" type="image/png" sizes="16x16" href="{{ asset('images/favicons/favicon-16x16.png') }}">
+    <link rel="manifest" href="{{ asset('images/favicons/fmanifest.json') }}">
+    <link rel="mask-icon" href="{{ asset('images/favicons/safari-pinned-tab.svg') }}" color="#5bbad5">
 
 <!-- Styles -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"

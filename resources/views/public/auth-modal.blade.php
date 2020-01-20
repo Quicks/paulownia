7942b72e31198265cf0f909f5a10a58d75a7fc61
s.stepaiko <stepaykosj@gmail.com>
2020-01-20 15:19:26 +0200

    Continue task#316 auth modal

--- resources/views/public/auth-modal.blade.php
+++ resources/views/public/auth-modal.blade.php
@@ -7,13 +7,13 @@
                     <li class="nav-item">
                         <a class="nav-link active" id="sign-tab" data-toggle="tab" href="#sign" кole="tab"
                              aria-controls="sign" aria-selected="true">
-                            Sign in
+                            Sign Up
                         </a>
                     </li>
                     <li class="nav-item">
                         <a class="nav-link" id="log-tab" data-toggle="tab" href="#log" role="tab" 
                                 aria-controls="log" aria-selected="false">
-                            login
+                            Login
                         </a>
                     </li>
                 </ul>
@@ -26,19 +26,62 @@
 
                 <div class="tab-content" id="myTabContent">
                     <div class="tab-pane fade show active" id="sign" role="tabpanel" aria-labelledby="sign-tab">
-                        <form method="POST" action="{{ route('customer.session.create') }}">
+                        <form method="post" action="{{ route('customer.register.create') }}">
                             {{ csrf_field() }}
                             <div class="login-form">
+                                <div class="control-group @if($errors->has('first_name')) has-error @endif">
+                                    <label for="first_name" class="required">{{ __('shop::app.customer.signup-form.firstname') }}</label>
+                                    <input type="text" class="control" name="first_name" required value="{{ old('first_name') }}">
+                                    <span class="control-error">{{ $errors->first('first_name') }}</span>
+                                </div>
 
-                                <div class="control-group" class="@if($errors->has('email')) has-error @endif">
-                                    <label for="email" class="required">{{ __('shop::app.customer.login-form.email') }}</label>
-                                    <input type="text" class="control" name="email" required value="{{ old('email') }}">
+                                <div class="control-group @if($errors->has('last_name')) has-error @endif">
+                                    <label for="last_name" class="required">{{ __('shop::app.customer.signup-form.lastname') }}</label>
+                                    <input type="text" class="control" name="last_name" required value="{{ old('last_name') }}">
+                                    <span class="control-error">{{ $errors->first('last_name') }}</span>
+                                </div>
+
+                                <div class="control-group @if($errors->has('email')) has-error @endif">
+                                    <label for="email" class="required">{{ __('shop::app.customer.signup-form.email') }}</label>
+                                    <input type="email" class="control" name="email" required value="{{ old('email') }}">
                                     <span class="control-error">{{ $errors->first('email') }}</span>
                                 </div>
 
-                                <div class="control-group" class="@if($errors->has('password')) has-error @endif">
+                                <div class="control-group @if($errors->has('password')) has-error @endif">
+                                    <label for="password" class="required">{{ __('shop::app.customer.signup-form.password') }}</label>
+                                    <input type="password" class="control" name="password" required minlength="6" value="{{ old('password') }}">
+                                    <span class="control-error">{{ $errors->first('password') }}</span>
+                                </div>
+
+                                <div class="control-group @if($errors->has('password_confirmation')) has-error @endif">
+                                    <label for="password_confirmation" class="required">{{ __('shop::app.customer.signup-form.confirm_pass') }}</label>
+                                    <input type="password" class="control" name="password_confirmation"  required minlength="6">
+                                    <span class="control-error">{{ $errors->first('password_confirmation') }}</span>
+                                </div>
+
+                                <div class="modal-footer">
+                                    <button class="btn btn-primary btn-lg" type="submit">
+                                        {{ __('shop::app.customer.signup-form.button_title') }}
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                        
+                    </div>
+
+                    <div class="tab-pane fade" id="log" role="tabpanel" aria-labelledby="log-tab">
+                        <form method="POST" action="{{ route('customer.session.create') }}">
+                            {{ csrf_field() }}
+                            <div class="login-form">
+                                <div class="control-group @if($errors->has('email')) has-error @endif">
+                                    <label for="email" class="required">{{ __('shop::app.customer.login-form.email') }}</label>
+                                    <input type="email" class="control" name="email" required value="{{ old('email') }}">
+                                    <span class="control-error">{{ $errors->first('email') }}</span>
+                                </div>
+
+                                <div class="control-group @if($errors->has('password')) has-error @endif">
                                     <label for="password" class="required">{{ __('shop::app.customer.login-form.password') }}</label>
-                                    <input type="password" class="control" name="password" required value="{{ old('password') }}">
+                                    <input type="password" class="control" name="password" required value="{{ old('password') }}" minlength="6">
                                     <span class="control-error">{{ $errors->first('password') }}</span>
                                 </div>
 
@@ -56,55 +99,10 @@
                                     </div>
                                 </div>
                             </div>
-                        </form>
 
-                        <div class="modal-footer">
-                            <input class="btn btn-primary btn-lg" type="submit" 
-                                    value="{{ __('shop::app.customer.login-form.button_title') }}">
-                        </div>
-
-                    </div>
-
-                    <div class="tab-pane fade" id="log" role="tabpanel" aria-labelledby="log-tab">
-                        <form method="post" action="{{ route('customer.register.create') }}">
-                            {{ csrf_field() }}
-
-                            <div class="login-form">
-
-                                <div class="control-group" :class="[errors.has('first_name') ? 'has-error' : '']">
-                                    <label for="first_name" class="required">{{ __('shop::app.customer.signup-form.firstname') }}</label>
-                                    <input type="text" class="control" name="first_name" v-validate="'required'" value="{{ old('first_name') }}" data-vv-as="&quot;{{ __('shop::app.customer.signup-form.firstname') }}&quot;">
-                                    <span class="control-error" v-if="errors.has('first_name')">@{{ errors.first('first_name') }}</span>
-                                </div>
-
-                                <div class="control-group" :class="[errors.has('last_name') ? 'has-error' : '']">
-                                    <label for="last_name" class="required">{{ __('shop::app.customer.signup-form.lastname') }}</label>
-                                    <input type="text" class="control" name="last_name" v-validate="'required'" value="{{ old('last_name') }}" data-vv-as="&quot;{{ __('shop::app.customer.signup-form.lastname') }}&quot;">
-                                    <span class="control-error" v-if="errors.has('last_name')">@{{ errors.first('last_name') }}</span>
-                                </div>
-
-                                <div class="control-group" :class="[errors.has('email') ? 'has-error' : '']">
-                                    <label for="email" class="required">{{ __('shop::app.customer.signup-form.email') }}</label>
-                                    <input type="email" class="control" name="email" v-validate="'required|email'" value="{{ old('email') }}" data-vv-as="&quot;{{ __('shop::app.customer.signup-form.email') }}&quot;">
-                                    <span class="control-error" v-if="errors.has('email')">@{{ errors.first('email') }}</span>
-                                </div>
-
-                                <div class="control-group" :class="[errors.has('password') ? 'has-error' : '']">
-                                    <label for="password" class="required">{{ __('shop::app.customer.signup-form.password') }}</label>
-                                    <input type="password" class="control" name="password" v-validate="'required|min:6'" ref="password" value="{{ old('password') }}" data-vv-as="&quot;{{ __('shop::app.customer.signup-form.password') }}&quot;">
-                                    <span class="control-error" v-if="errors.has('password')">@{{ errors.first('password') }}</span>
-                                </div>
-
-                                <div class="control-group" :class="[errors.has('password_confirmation') ? 'has-error' : '']">
-                                    <label for="password_confirmation" class="required">{{ __('shop::app.customer.signup-form.confirm_pass') }}</label>
-                                    <input type="password" class="control" name="password_confirmation"  v-validate="'required|min:6|confirmed:password'" data-vv-as="&quot;{{ __('shop::app.customer.signup-form.confirm_pass') }}&quot;">
-                                    <span class="control-error" v-if="errors.has('password_confirmation')">@{{ errors.first('password_confirmation') }}</span>
-                                </div>
-
-                                <button class="btn btn-primary btn-lg" type="submit">
-                                    {{ __('shop::app.customer.signup-form.button_title') }}
-                                </button>
-
+                            <div class="modal-footer">
+                                <input class="btn btn-primary btn-lg" type="submit" 
+                                        value="{{ __('shop::app.customer.login-form.button_title') }}">
                             </div>
                         </form>
                     </div>
@@ -114,3 +112,19 @@
         </div>
     </div>
 </div>
+
+
+@push('scripts')
+<script type="text/javascript">
+    $(document).ready(function () {
+        if( $('.has-error').length) {
+            $('#AuthModal').modal('show');
+        };
+        if({{old('first_name') ? 'true' : 'false'}}) {
+            $('#sign-tab').tab('show');
+        } else {
+            $('#log-tab').tab('show');
+        }
+    });
+</script>
+@endpush

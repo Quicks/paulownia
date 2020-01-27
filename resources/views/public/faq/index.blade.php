6b0fc08f23720e27782feed4fd84c7d7c71c2910
Orenchak <vitaorenchak@gmail.com>
2020-01-27 14:13:14 +0200

    create modal layout adaptively

--- resources/views/public/faq/index.blade.php
+++ resources/views/public/faq/index.blade.php
@@ -6,6 +6,55 @@
         <link rel="stylesheet" href="{{asset('css/contacts.css') }}?v1">
     @endpush
 
+
+
+
+    {{--<script type="text/javascript">--}}
+        {{--$(function () {--}}
+            {{--if ($('.wpfp-link').attr('title') === '8'){--}}
+                {{--$('.fa-heart').css({color: '#333333'});--}}
+                {{--// $('.fa-heart').css({backgroundImage:'images/button-faq-down.png'});--}}
+                {{--$('.fa-heart').css({backgroundColor: '#987'});--}}
+                {{--// $('.wpfp-link').css({backgroundImage:'/images/button-faq-down.png'});--}}
+            {{--}else{--}}
+                {{--$('.fa-heart').css({color: '#f71e1e'});--}}
+                {{--$('.fa-heart').css({backgroundColor: '#587'});--}}
+                {{--// $('.fa-heart').css({backgroundImage:'/images/button-faq-up.png'});--}}
+
+            {{--}--}}
+            {{--// допустим это выполняется скрипт плагина--}}
+            {{--$('.wpfp-link').on('click', function (e) {--}}
+                {{--e.preventDefault();--}}
+                {{--if ($(this).attr('title') === '8') {--}}
+                    {{--$(this).attr('title', '2');--}}
+                    {{--$(this).text('2');--}}
+                    {{--$('.wpfp-link').css({backgroundImage:'/images/button-faq-down.png'});--}}
+                {{--} else {--}}
+                    {{--$(this).attr('title', '8');--}}
+                    {{--$(this).text('8');--}}
+                    {{--$('.wpfp-link').css({backgroundImage:'/images/button-faq-up.png'});--}}
+                {{--}--}}
+            {{--});--}}
+            {{--// а это заменм цвета--}}
+            {{--$('.wpfp-link').on('click', function (e) {--}}
+                {{--e.preventDefault();--}}
+                {{--if ($(this).attr('title') === '8') {--}}
+                    {{--$('.fa-heart').css({color: '#333333'});--}}
+                    {{--$('.fa-heart').css({backgroundColor: '#987'});--}}
+                    {{--// $('.fa-heart').css({backgroundImage:'/images/button-faq-down.png'});--}}
+
+                {{--} else {--}}
+                    {{--$('.fa-heart').css({color: '#f71e1e'});--}}
+                    {{--$('.fa-heart').css({backgroundColor: '#587'});--}}
+                    {{--// $('.fa-heart').css({backgroundImage:'/images/button-faq-up.png'});--}}
+                {{--}--}}
+            {{--});--}}
+        {{--});--}}
+    {{--</script>--}}
+
+    {{--<i class="fa fa-heart" aria-hidden="true">test of edit</i>--}}
+    {{--<a class="wpfp-link" href="#" title="8" rel="nofollow"><img data-src="/images/button-faq-down.png" class="lazyload"></a>--}}
+
     <div class="row fon-faq">
 
         <div class="col-12 top-fon">@include('public.breadcrumbs', $breadcrumbs = [route('public.faq.index') => 'header-footer.faq' ])</div>
@@ -38,12 +87,34 @@
             <div class="accordion" id="accordionExample">
 
                 <div class="card">
+                    <div class="card-header" id="headingOne">
+                        <div class="row">
+                            <div class="col-11">Long question №1</div>
+                            <div class="col-1">
+                                <button class="btn btn-link collapsed " type="button" data-toggle="collapse" data-target="#collapseOne" aria-expanded="false" aria-controls="collapseOne">
+                                    <img data-src="/images/button-faq-down.png" class="lazyload " id="img-text-1" onClick="chg(id)">
+                                </button>
+                            </div>
+
+                        </div>
+                    </div>
+                    <div id="collapseOne" class="collapse" aria-labelledby="headingOne" data-parent="#accordionExample">
+                        <div class="card-body">
+                            Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card">
                     <div class="card-header" id="headingTwo">
-                        <h2 class="mb-0">
-                            <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
-                                Long question №1
-                            </button>
-                        </h2>
+                        <div class="row">
+                            <div class="col-11" >Long question №2</div>
+                                 <div class="col-1">
+                                     <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                                         <img data-src="/images/button-faq-down.png" class="lazyload " id="img-text-2" onClick="chg(id)">
+                                     </button>
+                                 </div>
+                        </div>
                     </div>
                     <div id="collapseTwo" class="collapse" aria-labelledby="headingTwo" data-parent="#accordionExample">
                         <div class="card-body">
@@ -51,13 +122,17 @@
                         </div>
                     </div>
                 </div>
+
                 <div class="card">
                     <div class="card-header" id="headingThree">
-                        <h2 class="mb-0">
-                            <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
-                                Long question №2
-                            </button>
-                        </h2>
+                        <div class="row">
+                            <div class="col-11">Long question №3</div>
+                                <div class="col-1">
+                                    <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                                        <img data-src="/images/button-faq-down.png" class="lazyload " id="img-text-3" onClick="chg(id)">
+                                    </button>
+                                </div>
+                        </div>
                     </div>
                     <div id="collapseThree" class="collapse" aria-labelledby="headingThree" data-parent="#accordionExample">
                         <div class="card-body">
@@ -65,30 +140,17 @@
                         </div>
                     </div>
                 </div>
-            </div>
 
-            <div class="card">
-                <div class="card-header" id="headingFour">
-                    <h2 class="mb-0">
-                        <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="#collapseFore" aria-expanded="false" aria-controls="collapseFore">
-                            Long question №3
-                        </button>
-                    </h2>
-                </div>
-                <div id="collapseFore" class="collapse" aria-labelledby="headingFore" data-parent="#accordionExample">
-                    <div class="card-body">
-                        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
-                    </div>
-                </div>
-            </div>
 
             <div class="card">
                 <div class="card-header no" id="headingFour" >
                            <div class="row">
-                               <div class="col-6 text-left" style="background:transparent;color: black" onclick="return ChangeColor(this);">Long question №4</div>
-                               <button class="btn btn-link collapsed col-6 text-right  " type="button" data-toggle="collapse" data-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
-                                   <img data-src="/images/button-faq-down.png" class="lazyload " id="img-text" onClick="chg(id)">
-                           </button>
+                               <div class="col-11" style="background:transparent;color: black" id="img-text-6" onClick="chg(id)">Long question №4</div>
+                                   <div class="col-1">
+                                       <button class="btn btn-link collapsed  " type="button" data-toggle="collapse" data-target="#collapseFive" aria-expanded="false" aria-controls="collapseFive">
+                                           <img data-src="/images/button-faq-down.png" class="lazyload " id="img-text" onClick="chg(id)">
+                                      </button>
+                                   </div>
                         </div>
 
                 </div>
@@ -99,6 +161,7 @@
                 </div>
             </div>
 
+        </div>
         </div>
         <div class="tab-pane fade" id="home-2" role="tabpanel" aria-labelledby="2-tab">...</div>
         <div class="tab-pane fade" id="home-3" role="tabpanel" aria-labelledby="3-tab">...</div>
@@ -130,33 +193,34 @@
     </div>
 
     <script>
-        // function ChangeColor(Element) {
-        //     if (Element.style.background || Element.style.color ==='transparent'||'black' )
-        //
-        //     {Element.style.background  = 'red';
-        //      Element.style.color  = 'white'}
-        //
-        //     else{Element.style.background = 'transparent';
-        //         Element.style.color = 'black'}
-        //     return false;
-        // }
-        // ___________________________________
-        // function ChangeColor(Element) {
-        //     if (document.getElementsByClassName('no').src.indexOf('red')>0)
-        //
-        //     { document.getElementsByClassName('yes').src='white'}
+        {{--// function ChangeColor(Element) {--}}
+        {{--//     if (Element.style.background || Element.style.color ==='transparent'||'black' )--}}
+        {{--//--}}
+        {{--//     {Element.style.background  = 'red';--}}
+        {{--//      Element.style.color  = 'white'}--}}
+        {{--//--}}
+        {{--//     else{Element.style.background = 'transparent';--}}
+        {{--//         Element.style.color = 'black'}--}}
+        {{--//     return false;--}}
+        {{--// }--}}
+        {{--// ___________________________________--}}
+        {{--// function ChangeColor(Element) {--}}
+        {{--//     if (document.getElementsByClassName('no').src.indexOf('red')>0)--}}
+        {{--//--}}
+        {{--//     { document.getElementsByClassName('yes').src='white'}--}}
 
-        // _____________________________________
+        {{--// _____________________________________--}}
 
-            // else{Element.style.background = 'transparent';
-            //     Element.style.color = 'black'}
-            // return false;
-        // }
+            {{--// else{Element.style.background = 'transparent';--}}
+            {{--//     Element.style.color = 'black'}--}}
+        {{--//     return false;--}}
+        {{--// }--}}
 
 
 
-        function chg(id){
-        if (document.getElementById(id).src.indexOf("/images/button-faq-down.png")>0){
+        async function chg(id){
+
+        if (document.getElementById(id).src.indexOf("/images/button-faq-down.png") > 0){
 
             document.getElementById(id).src="/images/button-faq-up.png"
         }else{

9085078a02538e63f2129d20bd425dc22fe2c5ac
Orenchak <vitaorenchak@gmail.com>
2020-02-03 23:08:28 +0200

    fix menu

--- resources/views/public/products/index.blade.php
+++ resources/views/public/products/index.blade.php
@@ -1,5 +1,11 @@
 @extends('layouts.public')
 @section('content')
+    <style>
+        .fon-text{
+            padding-top: 0 !important;
+        }
+    </style>
+
     @include('public.breadcrumbs', $breadcrumbs = [route('public.products.index') => 'header-footer.goods' ])
     @include('public.products.filter')
 @endsection

3a7c11720b142180f948cc1ddab6f7e8679854ea
s.stepaiko <stepaykosj@gmail.com>
2020-02-10 13:34:19 +0200

    Get types of paulownia label-text from DB

--- resources/views/public/paulownia/type.blade.php
+++ resources/views/public/paulownia/type.blade.php
@@ -62,17 +62,7 @@
                     aria-labelledby="nav-{{urlencode($type->admin_name)}}"
                     >
                     <div class="col-12 p-5 text-about-type">
-                        Paulownia Elongata (“Elongated”) is a fast-growing species with a straight and long trunk; in 5 years after a technical cut,
-                        it has the necessary size for the industrialization of wood. The height of paulownia Elongata reaches 21m,
-                        which is an order of magnitude greater than the previous varieties. The width of the crown belongs to the middle class,
-                        at which the landing is 6m x 5m and 6m x 6m. Heat-loving, the Mediterranean climate is more suitable for this tree,
-                        good for growing in warm countries: Italy, Spain, Croatia, North Africa, etc. Paulownia Elongata, along with Tomentosa,
-                        is a very strong honey plant, thousands of flowers appear on its vast crown, which are to the taste of bees.
-                        Beginning in 1970, China began to work on breeding various types of paulownia by crossing and planting in extreme conditions.
-                        Thanks to its rapid growth, it is excellent for planting biofuels and quality wood. It is necessary to take into account the fact
-                        that for the cultivation of this species or its clones, lifting mechanisms and high stepladders will be required to improve the upper part
-                        of the trunk and crown.
-                        Therefore, on the one hand, we get a little more wood than other species, but we also have more work with trimming and forming the upper part of the trunk.
+                        {{$type->label}}
                     </div>
                 </div>
             @endforeach

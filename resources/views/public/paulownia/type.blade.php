8b678653ad18472008330d653f3b3e3f072f6138
s.stepaiko <stepaykosj@gmail.com>
2020-02-10 13:19:35 +0200

    Get types of paulownia from DB

--- resources/views/public/paulownia/type.blade.php
+++ resources/views/public/paulownia/type.blade.php
@@ -38,43 +38,30 @@
                         </div>
                     </div>
             </div>
-        <div class="col-12 p-0 mx-auto justify-content-center fon-for-paulownia-type">
+        <div class="col-12 p-0 fon-for-paulownia-type">
             <nav>
-                <div class="nav nav-tabs" id="nav-tab" role="tablist">
-            <a class="nav-item nav-link active margin-for-type pt-5 pb-5" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true">Pao Tong</a>
-            <a class="nav-item nav-link margin-for-type pt-5  pb-5 " id="nav-tong-2-tab" data-toggle="tab" href="#nav-tong-2" role="tab" aria-controls="nav-tong-2" aria-selected="false">Shan Tong</a>
-            <a class="nav-item nav-link margin-for-type pt-5  pb-5 " id="nav-tong-3-tab" data-toggle="tab" href="#nav-tong-3" role="tab" aria-controls="nav-tong-3" aria-selected="false">Elongata</a>
-            <a class="nav-item nav-link margin-for-type pt-5  pb-5 " id="nav-tong-4-tab" data-toggle="tab" href="#nav-tong-4" role="tab" aria-controls="nav-tong-4" aria-selected="false">Tomentosa</a>
-            <a class="nav-item nav-link margin-for-type pt-5  pb-5 " id="nav-tong-5-tab" data-toggle="tab" href="#nav-tong-5" role="tab" aria-controls="nav-tong-5" aria-selected="false">Kawakamii</a>
-            <a class="nav-item nav-link margin-for-type pt-5  pb-5 " id="nav-tong-6-tab" data-toggle="tab" href="#nav-tong-6" role="tab" aria-controls="nav-tong-6" aria-selected="false">Turbo Pro</a>
-        </div>
+                <div class="nav nav-tabs justify-content-between px-4" id="nav-tab" role="tablist">
+                    @foreach($types as $type)
+                        <a class="nav-item nav-link  pt-5 pb-5 @if($loop->first) active show @endif" 
+                            data-toggle="tab" role="tab" 
+                            href="#nav-{{urlencode($type->admin_name)}}"
+                            aria-controls="nav-{{urlencode($type->admin_name)}}" 
+                            aria-selected="@if($loop->first) true @else false @endif"
+                            >
+                            {{$type->admin_name}}
+                        </a>
+                    @endforeach
+                </div>
             </nav>
 
         <div class="tab-content" id="nav-tabContent">
 
-            <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
-
-            <div class="col-12 p-5 text-about-type">
-
-                Paulownia Elongata (“Elongated”) is a fast-growing species with a straight and long trunk; in 5 years after a technical cut,
-                it has the necessary size for the industrialization of wood. The height of paulownia Elongata reaches 21m,
-                which is an order of magnitude greater than the previous varieties. The width of the crown belongs to the middle class,
-                at which the landing is 6m x 5m and 6m x 6m. Heat-loving, the Mediterranean climate is more suitable for this tree,
-                good for growing in warm countries: Italy, Spain, Croatia, North Africa, etc. Paulownia Elongata, along with Tomentosa,
-                is a very strong honey plant, thousands of flowers appear on its vast crown, which are to the taste of bees.
-                Beginning in 1970, China began to work on breeding various types of paulownia by crossing and planting in extreme conditions.
-                Thanks to its rapid growth, it is excellent for planting biofuels and quality wood. It is necessary to take into account the fact
-                that for the cultivation of this species or its clones, lifting mechanisms and high stepladders will be required to improve the upper part
-                of the trunk and crown.
-                Therefore, on the one hand, we get a little more wood than other species, but we also have more work with trimming and forming the upper part of the trunk.
-
-            </div>
-
-            </div>
-                <div class="tab-pane fade" id="nav-tong-2" role="tabpanel" aria-labelledby="nav-tong-2-tab">
-
+            @foreach($types as $type)
+                <div class="tab-pane fade @if($loop->first) active show @endif" 
+                    id="nav-{{urlencode($type->admin_name)}}" role="tabpanel" 
+                    aria-labelledby="nav-{{urlencode($type->admin_name)}}"
+                    >
                     <div class="col-12 p-5 text-about-type">
-
                         Paulownia Elongata (“Elongated”) is a fast-growing species with a straight and long trunk; in 5 years after a technical cut,
                         it has the necessary size for the industrialization of wood. The height of paulownia Elongata reaches 21m,
                         which is an order of magnitude greater than the previous varieties. The width of the crown belongs to the middle class,
@@ -86,81 +73,11 @@
                         that for the cultivation of this species or its clones, lifting mechanisms and high stepladders will be required to improve the upper part
                         of the trunk and crown.
                         Therefore, on the one hand, we get a little more wood than other species, but we also have more work with trimming and forming the upper part of the trunk.
-
-                    </div>
-
-                </div>
-                <div class="tab-pane fade" id="nav-tong-3" role="tabpanel" aria-labelledby="nav-tong-3-tab">
-
-                    <div class="col-12 p-5 text-about-type">
-
-                        Paulownia Elongata (“Elongated”) is a fast-growing species with a straight and long trunk; in 5 years after a technical cut,
-                        it has the necessary size for the industrialization of wood. The height of paulownia Elongata reaches 21m,
-                        which is an order of magnitude greater than the previous varieties. The width of the crown belongs to the middle class,
-                        at which the landing is 6m x 5m and 6m x 6m. Heat-loving, the Mediterranean climate is more suitable for this tree,
-                        good for growing in warm countries: Italy, Spain, Croatia, North Africa, etc. Paulownia Elongata, along with Tomentosa,
-                        is a very strong honey plant, thousands of flowers appear on its vast crown, which are to the taste of bees.
-                        Beginning in 1970, China began to work on breeding various types of paulownia by crossing and planting in extreme conditions.
-                        Thanks to its rapid growth, it is excellent for planting biofuels and quality wood. It is necessary to take into account the fact
-                        that for the cultivation of this species or its clones, lifting mechanisms and high stepladders will be required to improve the upper part
-                        of the trunk and crown.
-                        Therefore, on the one hand, we get a little more wood than other species, but we also have more work with trimming and forming the upper part of the trunk.
-
-                    </div>
-
-                    .</div>
-                <div class="tab-pane fade" id="nav-tong-4" role="tabpanel" aria-labelledby="nav-tong-4-tab">
-                    <div class="col-12 p-5 text-about-type">
-
-                        Paulownia Elongata (“Elongated”) is a fast-growing species with a straight and long trunk; in 5 years after a technical cut,
-                        it has the necessary size for the industrialization of wood. The height of paulownia Elongata reaches 21m,
-                        which is an order of magnitude greater than the previous varieties. The width of the crown belongs to the middle class,
-                        at which the landing is 6m x 5m and 6m x 6m. Heat-loving, the Mediterranean climate is more suitable for this tree,
-                        good for growing in warm countries: Italy, Spain, Croatia, North Africa, etc. Paulownia Elongata, along with Tomentosa,
-                        is a very strong honey plant, thousands of flowers appear on its vast crown, which are to the taste of bees.
-                        Beginning in 1970, China began to work on breeding various types of paulownia by crossing and planting in extreme conditions.
-                        Thanks to its rapid growth, it is excellent for planting biofuels and quality wood. It is necessary to take into account the fact
-                        that for the cultivation of this species or its clones, lifting mechanisms and high stepladders will be required to improve the upper part
-                        of the trunk and crown.
-                        Therefore, on the one hand, we get a little more wood than other species, but we also have more work with trimming and forming the upper part of the trunk.
-
                     </div>
                 </div>
-                <div class="tab-pane fade" id="nav-tong-5" role="tabpanel" aria-labelledby="nav-tong-5-tab">
-                    <div class="col-12 p-5 text-about-type">
+            @endforeach
 
-                        Paulownia Elongata (“Elongated”) is a fast-growing species with a straight and long trunk; in 5 years after a technical cut,
-                        it has the necessary size for the industrialization of wood. The height of paulownia Elongata reaches 21m,
-                        which is an order of magnitude greater than the previous varieties. The width of the crown belongs to the middle class,
-                        at which the landing is 6m x 5m and 6m x 6m. Heat-loving, the Mediterranean climate is more suitable for this tree,
-                        good for growing in warm countries: Italy, Spain, Croatia, North Africa, etc. Paulownia Elongata, along with Tomentosa,
-                        is a very strong honey plant, thousands of flowers appear on its vast crown, which are to the taste of bees.
-                        Beginning in 1970, China began to work on breeding various types of paulownia by crossing and planting in extreme conditions.
-                        Thanks to its rapid growth, it is excellent for planting biofuels and quality wood. It is necessary to take into account the fact
-                        that for the cultivation of this species or its clones, lifting mechanisms and high stepladders will be required to improve the upper part
-                        of the trunk and crown.
-                        Therefore, on the one hand, we get a little more wood than other species, but we also have more work with trimming and forming the upper part of the trunk.
-
-                    </div>
-                </div>
-                <div class="tab-pane fade" id="nav-tong-6" role="tabpanel" aria-labelledby="nav-tong-6-tab">
-                    <div class="col-12 p-5 text-about-type">
-
-                        Paulownia Elongata (“Elongated”) is a fast-growing species with a straight and long trunk; in 5 years after a technical cut,
-                        it has the necessary size for the industrialization of wood. The height of paulownia Elongata reaches 21m,
-                        which is an order of magnitude greater than the previous varieties. The width of the crown belongs to the middle class,
-                        at which the landing is 6m x 5m and 6m x 6m. Heat-loving, the Mediterranean climate is more suitable for this tree,
-                        good for growing in warm countries: Italy, Spain, Croatia, North Africa, etc. Paulownia Elongata, along with Tomentosa,
-                        is a very strong honey plant, thousands of flowers appear on its vast crown, which are to the taste of bees.
-                        Beginning in 1970, China began to work on breeding various types of paulownia by crossing and planting in extreme conditions.
-                        Thanks to its rapid growth, it is excellent for planting biofuels and quality wood. It is necessary to take into account the fact
-                        that for the cultivation of this species or its clones, lifting mechanisms and high stepladders will be required to improve the upper part
-                        of the trunk and crown.
-                        Therefore, on the one hand, we get a little more wood than other species, but we also have more work with trimming and forming the upper part of the trunk.
-
-                    </div>
-                </div>
-            </div>
+        </div>
 
 
         <div class="col-12 text-center">

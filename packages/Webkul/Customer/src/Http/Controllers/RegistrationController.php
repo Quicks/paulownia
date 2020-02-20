0d6721614ff05157d640ccf1cbd567b73c02356f
s.stepaiko <stepaykosj@gmail.com>
2020-02-20 21:46:36 +0200

    Autologin after registration

--- packages/Webkul/Customer/src/Http/Controllers/RegistrationController.php
+++ packages/Webkul/Customer/src/Http/Controllers/RegistrationController.php
@@ -106,7 +106,7 @@ class RegistrationController extends Controller
             if($request->has('subscribe')) {
                 $this->subscribe();
             }
-
+            auth()->guard('customer')->attempt(request(['email', 'password']));  //THIS IS AUTOLOGIN
             return redirect()->back(); //redirect()->route($this->_config['redirect']);
         } else {
             session()->flash('error', trans('shop::app.customer.signup-form.failed'));

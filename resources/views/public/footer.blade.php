8be4a7c907714c233b7828ed9dc31cb9ef84ec72
Orenchak <vitaorenchak@gmail.com>
2020-03-07 14:23:16 +0200

    download booklets

--- resources/views/public/footer.blade.php
+++ resources/views/public/footer.blade.php
@@ -58,7 +58,12 @@
                 <img class="mt-4 ml-xl-4 mx-auto footer-booklet" src="{{asset('images/booklet_en.jpg')}}">
             </div>
             <div>
-                <a href="#"><img class="f-booklet-download lazyload ml-xl-4 mt-4" data-src="{{asset('images/download-booklet.svg')}}" ></a>
+                <a @if(app()->getLocale() == 'fr' || app()->getLocale() == 'pl' || app()->getLocale() == 'ar')
+                   href="{{asset('/files/booklet_en.pdf')}}"
+                   @else href="{{asset('/files/booklet_' . app()->getLocale()  .  '.pdf')}}"
+                   @endif download >
+                    <img class="f-booklet-download lazyload ml-xl-4 mt-4" data-src="{{asset('images/download-booklet.svg')}}" >
+                </a>
             </div>
         </div>
     </div>

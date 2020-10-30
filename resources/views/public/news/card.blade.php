121010c7cd180aeaac6b71ff24842c35881cb023
Quicks <1989alpan@gmail.com>
2020-10-30 21:27:04 +0200

    add possibility to views news page

--- resources/views/public/news/card.blade.php
+++ resources/views/public/news/card.blade.php
@@ -1,19 +1,19 @@
-<div class="col-lg-4 col-md-6">
-    <div class="blog_post blog_style1 radius_all_10 animation" data-animation="fadeInUp" data-animation-delay="0.05s">
-        <div class="blog_img">
-            <?php $imagesCount = count($newsItem->images()->get()); ?>
-                @if($imagesCount > 1)
+<div class="col-md-3">
+    <div class="blog_post blog_style1 animation" data-animation="fadeInUp" data-animation-delay="0.05s">
+        
+        <div class="blog_img round-corners">
+                @if(!empty($newsItem->images))
                     <div class="carousel_slide1 owl-carousel owl-theme" data-autoplay="false" data-autoheight="true" data-loop="true" data-nav="true" data-dots="false" data-autoplay-timeout="3000">
                         @foreach($newsItem->images()->get() as $img)
-                            <a href="{{route('public.news.show', [mb_strtolower(class_basename($newsItem)), $newsItem->id])}}">
+                            <a href="{{route('public.news.show', [mb_strtolower(class_basename($newsItem)), $newsItem->id])}}" class='round-corners'>
                                 <img src="/storage/{{$img->image230}}" alt="blog_img4">
                             </a>
                         @endforeach
                     </div>
                 @else
-                    <a href="{{route('public.news.show', [mb_strtolower(class_basename($newsItem)), $newsItem->id])}}">
+                    <a href="{{route('public.news.show', [mb_strtolower(class_basename($newsItem)), $newsItem->id])}}" class='round-corners'>
                       @if($imagesCount == 1)
-                          <img src="/storage/{{$newsItem->images->first()->image230}}" alt="blog_small_img2">
+                          <img src="/storage/{{$newsItem->images->first()->image230}}" alt="blog_small_img2"/>
                       @else
                           <img src="/images/product_img1.jpg" alt="product_img1"/>
                       @endif
@@ -22,17 +22,28 @@
 
         </div>
         <div class="blog_content">
-            <h6 class="blog_title"><a href="{{route('public.news.show', [mb_strtolower(class_basename($newsItem)), $newsItem->id])}}">{{substr($newsItem->title, 0, 100)}}</a></h6>
+            <h6 class="blog_title">
+                <a href="{{route('public.news.show', [mb_strtolower(class_basename($newsItem)), $newsItem->id])}}" class='title'>
+                    {{substr($newsItem->title, 0, 100)}}
+                </a>
+            </h6>
             <ul class="list_none blog_meta">
-                <li><a href="javascript:void(0);"><i class="far fa-calendar"></i>{{date('F j, Y', strtotime($newsItem->publish_date))}}</a></li>
                 <li>
+                    <a href="javascript:void(0);">{{date('F j Y', strtotime($newsItem->publish_date))}}</a>
+                </li>
+                <!-- <li>
                     <a href="{{route('public.news.show', [mb_strtolower(class_basename($newsItem)), $newsItem->id])}}#comments"><i class="far fa-comments"></i>
                         {{count($newsItem->comments)}} {{ __('news.comment')}}
                     </a>
-                </li>
+                </li> -->
             </ul>
-            <p>{!! substr(strip_tags($newsItem->text), 0, 200) !!}</p>
-            <a href="{{route('public.news.show', [mb_strtolower(class_basename($newsItem)), $newsItem->id])}}" class="blog_link">{{ __('news.more')}}<i class="ion-ios-arrow-right"></i></a>
+            <div class='blog_short_desc'>
+                {!! substr(strip_tags($newsItem->text), 0, 300) !!}
+                <a href="{{route('public.news.show', [mb_strtolower(class_basename($newsItem)), $newsItem->id])}}" class="blog_link">
+                    ...{{ __('news.more')}}
+                </a>
+            </div>
+            
         </div>
     </div>
 </div>
--- resources/views/public/news/show.blade.php
+++ resources/views/public/news/show.blade.php
@@ -1,136 +1,82 @@
 @extends('layouts.public')
-
 @section('content')
-    <!-- START SECTION BANNER -->
-    @include('public.breadcrumbs', [
-        $breadcrumbs = [
-            route('public.news.index') => 'header-footer.news',
-            route('public.news.show') => $news->title,
-
-        ],
-        $pageTitle = $news->title
-    ])
-    <!-- END SECTION BANNER -->
-    <section>
-        <div class="container">
-            <div class="row">
-                <div class="col-12">
-                    <div class="single_post">
-                        <div class="blog_img">
-                            <a href="#">
-                                @if(isset($news->images()->first()->image1200))
-                                    <img src="/storage/{{$news->images()->first()->image1200}}" alt="blog_small_img2">
+@include('public.blocks.page_header', ['title' => __('header-footer.news')])
+    <div class='custom-page-description'>
+        <section>
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="single_post">
+                            <div class="blog_img">
+                                @if(isset($news->images()->first()->image230))
+                                    <img src="/storage/{{$news->images()->first()->image230}}" alt="blog_small_img2">
                                 @else
                                     <img src="/images/product_img1.jpg" alt="product_img1"/>
                                 @endif
-                            </a>
-                        </div>
-                        <div class="single_post_content">
-                            <div class="blog_text">
-                                <h2 class="blog_title">{{$news->title}}</h2>
-                                <ul class="list_none blog_meta">
-                                    <li>
-                                        <a href="javascript:void(0);">
-                                            <i class="far fa-calendar"></i>
-                                            {{date('F j, Y', strtotime($news->publish_date))}}
-                                        </a>
-                                    </li>
-                                    {{--                                    <li><a href="#"><i class="far fa-user"></i>by <span class="text_default">admin</span></a></li>--}}
-                                    <li><a href="#comments"><i class="far fa-comments"></i>{{count($news->comments)}} {{ __('news.comment')}}</a></li>
-                                </ul>
-                                <p>{!!$news->text!!}</p>
-                                <div class="border-top border-bottom blog_post_footer">
-                                    <div class="row justify-content-end align-items-center">
-{{--                                        <div class="col-md-8 mb-3 mb-md-0">--}}
-{{--                                            <div class="tags">--}}
-{{--                                                <a href="#">General</a>--}}
-{{--                                                <a href="#">Design</a>--}}
-{{--                                                <a href="#">jQuery</a>--}}
-{{--                                                <a href="#">Branding</a>--}}
-{{--                                                <a href="#">Modern</a>--}}
-{{--                                            </div>--}}
-{{--                                        </div>--}}
-                                        <div class="col-md-4 text-md-right">
-                                            <div class="share">
-                                                <ul class="list_none social_icons">
-                                                    <li><a href="#" class="sc_facebook"><i class="ion-social-facebook"></i></a></li>
-                                                    <li><a href="#" class="sc_twitter"><i class="ion-social-twitter"></i></a></li>
-                                                    <li><a href="#" class="sc_gplus"><i class="ion-social-googleplus"></i></a></li>
-{{--                                                    <li><a href="#" class="sc_youtube"><i class="ion-social-youtube-outline"></i></a></li>--}}
-                                                    <li><a href="#" class="sc_instagram"><i class="ion-social-instagram-outline"></i></a></li>
-                                                </ul>
-                                            </div>
-                                        </div>
+                            </div>
+                            <div class="single_post_content">
+                                <div class="blog_text">
+                                    <h2 class="blog_title">{{$news->title}}</h2>
+                                    <ul class="list_none blog_meta">
+                                        <li>
+                                            <a href="javascript:void(0);">
+                                                <i class="far fa-calendar"></i>
+                                                {{date('F j, Y', strtotime($news->publish_date))}}
+                                            </a>
+                                        </li>
+                                    </ul>
+                                    <p>{!!$news->text!!}</p>
+                                    <div class='blog_images'>
+                                        @if(isset($news->images()->first()->image1200))
+                                            @foreach($news->images as $image)
+                                                <img src="/storage/{{$image->image1200}}" alt="blog_small_img2">
+                                            @endforeach
+                                        @endif
                                     </div>
                                 </div>
                             </div>
-                        </div>
-                        <div class="post_navigation">
-                            <div class="row align-items-center justify-content-between">
-                                <div class="col-auto">
-                                    @if (isset($previous))
-                                        <a href="{{route('public.news.show', [mb_strtolower(class_basename($previous)), $previous->id])}}">
-                                            <div class="d-flex align-items-center">
-                                                <i class="ion-ios-arrow-thin-left mr-2"></i>
-                                                <div>
-                                                    <span>{{ __('news.previous_post')}}</span>
-                                                </div>
-                                            </div>
-                                        </a>
-                                    @endif
-                                </div>
-                                <div class="col-auto">
-                                    @if (isset($next))
-                                        <a href="{{route('public.news.show', [mb_strtolower(class_basename($next)), $next->id])}}">
-                                            <div class="d-flex align-items-center flex-row-reverse text-right">
-                                                <i class="ion-ios-arrow-thin-right ml-2"></i>
-                                                <div>
-                                                    <span>{{ __('news.next_post')}}</span>
-                                                </div>
-                                            </div>
-                                        </a>
-                                    @endif
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="comment-area" id="comments">
-                            <div class="posts-title">
-                                <h5>({{count($news->comments)}}) {{ __('comments.comment')}}</h5>
-                            </div>
-                            @if($news->comments()->parents()->get())
-                                @include('public.comments.index', [
-                                    'comments' => $news->comments()->parents()->get(),
-                                    'model' => $news
-                                ])
-                            @endif
-                            @if(Auth::guard('customer')->check())
-                                <div class="posts-title">
-                                    <h5>{{ __('comments.write_a_comment')}}</h5>
-                                </div>
-                                @include('public.comments.form', [
-                                    'commentable_id' => $news->id,
-                                    'commentable_type' => get_class($news),
-                                    'parent_id' => 0,
-                                ])
-                            @else
-                                <div class="field_form form_style2">
-                                    <div class="posts-title">
-                                        <h5>{{ __('comments.leave_comment')}}</h5>
-                                    </div>
-                                    <div class="posts-title">
-                                        {{ __('comments.leave_comment_message_begin')}}
-                                        <a href="/customer/login">{{ __('comments.leave_comment_message_logged_in')}}</a>
-                                        {{ __('comments.leave_comment_message_end')}}
-                                    </div>
-                                </div>
-                            @endif
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </section>
-    <!-- END SECTION BLOG -->
-
+        </section>
+    </div>
 @endsection
+@push('scripts')
+
+<script>
+    $(document).ready(function(){
+        if($('.blog_images img').get().length > 2){
+            $('.blog_images').slick({
+                centerMode: true,
+                dots: true,
+                lazyLoad: 'ondemand',
+                autoplay: true,
+                autoplaySpeed: 2000,
+                slidesToShow: 3,
+                responsive: [
+                    {
+                    breakpoint: 768,
+                    settings: {
+                        arrows: false,
+                        centerMode: true,
+                        centerPadding: '40px',
+                        slidesToShow: 3
+                    }
+                    },
+                    {
+                    breakpoint: 480,
+                    settings: {
+                        arrows: false,
+                        centerMode: true,
+                        centerPadding: '40px',
+                        slidesToShow: 1
+                    }
+                    }
+                ]
+                });
+                
+        }
+    })
+</script>
+@endpush
